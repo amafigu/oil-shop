42bb2df75dafdecb7e9a06585c0af30dc4e61931
amafigu <amadeusfigue@gmail.com>
2023-05-18 20:16:14 +0200

    call young living api

--- frontend/src/components/Sidebar/Sidebar.js
+++ frontend/src/components/Sidebar/Sidebar.js
@@ -1,16 +1,34 @@
 import React from "react"
 import style from "./Sidebar.module.css"
 import { SidebarItemList } from "../../app/components/SidebarItemList"
+import axios from "axios"
 
 const Sidebar = () => {
-  
+  function callYL() {
+    const username = "your_username"
+    const password = "your_password"
+
+    axios
+      .post("https://www.youngliving.com/api/accounts/token", {
+        userName: username,
+        email: username,
+        memberId: username,
+        password: password,
+        rememberMe: true,
+      })
+      .then((response) => {
+        console.log("response.data ", response.data)
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  }
+
   return (
     <div>
-    <ul className={style.sidebar}>
-      {SidebarItemList.map((i) => {
-        return (
-          <li className={style.sidebarItem}>{i.label}</li>
-          )
+      <ul className={style.sidebar}>
+        {SidebarItemList.map((i) => {
+          return <li className={style.sidebarItem}>{i.label}</li>
         })}
       </ul>
     </div>
