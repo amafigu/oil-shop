203e6482429a9e1f4b6d98d06f2470669cca9957
Amadeus Figueiredo <amadeusfigue@gmail.com>
2023-08-18 15:22:00 +0200

    fix list and items spacing

--- frontend/src/pages/Cart.js
+++ frontend/src/pages/Cart.js
@@ -33,43 +33,45 @@ const Cart = () => {
                   <p>{item.product.description}</p>
                   <p>{item.product.size} ml</p>
                 </div>
-                <div className={styles.cartItemQuantitySelector}>
-                  <button
-                    className={styles.cartItemQuantityButton}
-                    onClick={() =>
-                      updateProductQuantity(
-                        item.product.name,
-                        item.quantity - 1,
-                      )
-                    }
-                  >
-                    -
-                  </button>
-                  <span className={styles.cartItemQuantityInput}>
-                    {item.quantity}
-                  </span>
-                  <button
-                    className={styles.cartItemQuantityButton}
-                    onClick={() =>
-                      updateProductQuantity(
-                        item.product.name,
-                        item.quantity + 1,
-                      )
-                    }
-                  >
-                    +
-                  </button>
+                <div className={styles.cartItemSelectors}>
+                  <div className={styles.quantityButtonsContainer}>
+                    <button
+                      className={styles.cartItemQuantityButton}
+                      onClick={() =>
+                        updateProductQuantity(
+                          item.product.name,
+                          item.quantity - 1,
+                        )
+                      }
+                    >
+                      -
+                    </button>
+                    <span className={styles.cartItemQuantityInput}>
+                      {item.quantity}
+                    </span>
+                    <button
+                      className={styles.cartItemQuantityButton}
+                      onClick={() =>
+                        updateProductQuantity(
+                          item.product.name,
+                          item.quantity + 1,
+                        )
+                      }
+                    >
+                      +
+                    </button>
+                  </div>
                   <div className={styles.cartItemTotalCost}>
                     {(item.quantity * Number(item.product.price)).toFixed(2)} €
                   </div>
-                </div>
-                <div className={styles.deleteButtonWrapper}>
-                  <span
-                    className={styles.deleteButton}
-                    onClick={() => removeProduct(item.product.name)}
-                  >
-                    {text.deleteButton}
-                  </span>
+                  <div className={styles.deleteButtonWrapper}>
+                    <span
+                      className={styles.deleteButton}
+                      onClick={() => removeProduct(item.product.name)}
+                    >
+                      {text.deleteButton}
+                    </span>
+                  </div>
                 </div>
               </div>
               <hr />
