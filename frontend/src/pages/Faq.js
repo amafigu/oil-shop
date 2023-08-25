9761937b2692a34a461027e97b952b6f593b9e88
Amadeus Figueiredo <amadeusfigue@gmail.com>
2023-08-25 15:36:27 +0200

    faq-styles

--- frontend/src/pages/Faq.js
+++ frontend/src/pages/Faq.js
@@ -8,27 +8,48 @@ const Faq = () => {
 
   useEffectScrollTop()
   return (
-    <div className={styles.pageWrapper}>
-      <div className={styles.page}>
-        <p>{translate.pages.faq.firstQuestion}</p>
-        <p>{translate.pages.faq.firstAnswer}</p>
-        <p>{translate.pages.faq.secondQuestion}</p>
-        <p>{translate.pages.faq.secondAnswer}</p>
-        <p>{translate.pages.faq.thirdQuestion}</p>
-        <p>{translate.pages.faq.thirdAnswer}</p>
-        <p>{translate.pages.faq.fourthQuestion} </p>
-        <p>{translate.pages.faq.fourthAnswer}</p>
-        <p>{translate.pages.faq.fifthAnswer}</p>
-        <p>{translate.pages.faq.fifthQuestion}</p>
-        <p>{translate.pages.faq.sixthQuestion}</p>
-        <p>{translate.pages.faq.sixthAnswer}</p>
-        <p>{translate.pages.faq.seventhAnswer}</p>
-        <p>{translate.pages.faq.eighthQuestion}</p>
-        <p>{translate.pages.faq.eighthAnswer}</p>
-        <p>{translate.pages.faq.ninthQuestion}</p>
-        <p>{translate.pages.faq.ninthAnswer}</p>
-        <p>{translate.pages.faq.tenthQuestion}</p>
-        <p>{translate.pages.faq.tenthAnswer}</p>
+    <div className={styles.faqPageWrapper}>
+      <div className={styles.faqPage}>
+        <div className={styles.questionsAndAnswers}>
+          <p className={styles.question}>
+            {translate.pages.faq.difuserRoomSizeQuestion}
+          </p>
+          <p className={styles.answer}>
+            {translate.pages.faq.difuserRoomSizeAnswer}
+          </p>
+          <p className={styles.question}>
+            {translate.pages.faq.howDifuserWorksQuestion}
+          </p>
+          <p className={styles.answer}>
+            {translate.pages.faq.howDifuserWorksAnswer}
+          </p>
+          <p className={styles.question}>{translate.pages.faq.thirdQuestion}</p>
+          <p className={styles.answer}>{translate.pages.faq.thirdAnswer}</p>
+          <p className={styles.question}>
+            {translate.pages.faq.fourthQuestion}
+          </p>
+          <p className={styles.answer}>{translate.pages.faq.fourthAnswer}</p>
+          <p className={styles.question}>
+            {translate.pages.faq.addBodyOilAndUsedAsCosmeticQuestion}
+          </p>
+          <p className={styles.answer}>
+            {translate.pages.faq.addBodyOilAndUsedAsCosmeticAnswer}
+          </p>
+          <p className={styles.question}>{translate.pages.faq.sixthQuestion}</p>
+          <p className={styles.answer}>{translate.pages.faq.sixthAnswer}</p>
+          <p className={styles.question}>
+            {translate.pages.faq.seventhQuestion}
+          </p>
+          <p className={styles.answer}>{translate.pages.faq.seventhAnswer}</p>
+          <p className={styles.question}>
+            {translate.pages.faq.eighthQuestion}
+          </p>
+          <p className={styles.answer}>{translate.pages.faq.eighthAnswer}</p>
+          <p className={styles.question}>{translate.pages.faq.ninthQuestion}</p>
+          <p className={styles.answer}>{translate.pages.faq.ninthAnswer}</p>
+          <p className={styles.question}>{translate.pages.faq.tenthQuestion}</p>
+          <p className={styles.answer}>{translate.pages.faq.tenthAnswer}</p>
+        </div>
       </div>
     </div>
   )
