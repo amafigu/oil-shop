1666665cfd028e358d1e8725d5aa05ffcc47d5a7
Amadeus Figueiredo <amadeusfigue@gmail.com>
2023-09-29 23:48:18 +0200

    adjust video teaser

--- frontend/src/pages/Home/Carousel.js
+++ frontend/src/pages/Home/Carousel.js
@@ -1,10 +1,5 @@
 import { TEASER_VIDEOS } from "#utils/constants"
-import {
-  faChevronLeft,
-  faChevronRight,
-} from "@fortawesome/free-solid-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import React, { useCallback, useEffect, useState } from "react"
+import React, { useCallback, useEffect, useRef, useState } from "react"
 import styles from "./carousel.module.scss"
 
 const Carousel = () => {
@@ -15,11 +10,6 @@ const Carousel = () => {
     setCurrentVideoIndex(newIndex >= TEASER_VIDEOS.length ? 0 : newIndex)
   }, [currentVideoIndex])
 
-  const previousVideo = useCallback(() => {
-    const newIndex = currentVideoIndex - 1
-    setCurrentVideoIndex(newIndex < 0 ? TEASER_VIDEOS.length - 1 : newIndex)
-  }, [currentVideoIndex])
-
   useEffect(() => {
     const timer = setInterval(() => {
       nextVideo()
@@ -28,28 +18,34 @@ const Carousel = () => {
     return () => clearInterval(timer)
   }, [nextVideo])
 
+  const videoRef = useRef(null)
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.play()
+    }
+  }, [])
+
   const currentVideo = TEASER_VIDEOS[currentVideoIndex]
-  const videoSrc = `https://www.youtube.com/embed/${currentVideo.id}?start=${currentVideo.start}&end=${currentVideo.end}&autoplay=1&loop=1&playlist=${currentVideo.id}&controls=0&modestbranding=1&mute=1`
+  const videoSrc = `${process.env.PUBLIC_URL}/assets/${currentVideo.id}.mp4`
 
   return (
     <div className={styles.carouselWrapper}>
-      <button className={styles.iconSlider} onClick={previousVideo}>
-        <FontAwesomeIcon icon={faChevronLeft} />
-      </button>
-      <div className={styles.videoWrapper}>
-        <iframe
-          key={videoSrc}
-          className={styles.iframeVideo}
-          src={videoSrc}
-          title='teaser video'
-          allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-        ></iframe>
-        <div className={styles.borderDiv}></div>
+      <div className={styles.outerContainer}>
+        <div className={styles.videoWrapper}>
+          <video
+            ref={videoRef}
+            autoPlay
+            muted
+            playsInline
+            loop
+            className={styles.video}
+            src={videoSrc}
+            title='teaser video'
+            type='video/mp4'
+          ></video>
+        </div>
       </div>
-
-      <button onClick={nextVideo} className={styles.iconSlider}>
-        <FontAwesomeIcon icon={faChevronRight} />
-      </button>
     </div>
   )
 }
