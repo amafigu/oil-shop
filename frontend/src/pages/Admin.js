678c4875c2ca5c7d1ae77b411f76608b10f4ba9c
Amadeus Figueiredo <amadeusfigue@gmail.com>
2023-10-10 13:55:55 +0200

    admin page ui

--- frontend/src/pages/Admin.js
+++ frontend/src/pages/Admin.js
@@ -1,23 +1,25 @@
 import axios from "axios"
+
+import { titleCase } from "#utils/utils"
 import React, { useEffect, useState } from "react"
-import style from "./admin.module.scss"
+import NotificationCard from "../components/NotificationCard"
+import styles from "./admin.module.scss"
 
 const Admin = () => {
   const [adminData, setAdminData] = useState(null)
-  const [productData, setProductData] = useState({})
-  const [productCategoryId, setProductCategoryId] = useState(null)
+  const [notification, setNotification] = useState(null)
+  const [productData, setProductData] = useState({
+    name: "",
+    productCategoryId: "",
+    description: "",
+    price: "",
+    details: "",
+    measure: "",
+    image: "",
+    size: "",
+  })
 
-  /*
-  const [productName, setProductName] = useState("")
-
-  const [productPrice, setProductPrice] = useState(0)
-  const [productDescription, setProductDescription] = useState("")
-  const [productDetails, setProductDetails] = useState("")
-  const [productMeasure, setProductMeasure] = useState("")
-  const [productImageUrl, setProductImageUrl] = useState("")
-  const [productSize, setProductSize] = useState(0)
-
-  */
+  const [productCategories, setProductCategories] = useState(null)
 
   useEffect(() => {
     const fetchAdminData = async () => {
@@ -37,13 +39,14 @@ const Admin = () => {
 
   useEffect(() => {
     try {
-      const getProductCategoryId = async () => {
+      const getProductCategories = async () => {
         const response = await axios.get(
           `${process.env.REACT_APP_API_URL}/product-categories`,
         )
+        setProductCategories(response)
         console.log("getProductsCat ", response)
       }
-      getProductCategoryId()
+      getProductCategories()
     } catch (error) {
       console.error("Can not get product categories ", error)
     }
@@ -54,18 +57,12 @@ const Admin = () => {
   }, [productData])
 
   const listenInputChange = (e) => {
-    const { name, value } = e.target
-
-    let valueToCheck = value
+    let valueToCheck = e.target.value
     if (e.target.type === "number") {
-      valueToCheck = parseFloat(value)
+      valueToCheck = parseFloat(e.target.value)
     }
 
-    console.log("listenInputChange e.target ", e.target)
-    setProductData((previousState) => ({
-      ...previousState,
-      [name]: valueToCheck,
-    }))
+    setProductData({ ...productData, [e.target.name]: e.target.value })
   }
 
   const uploadImageToBucket = () => {
@@ -81,59 +78,120 @@ const Admin = () => {
         productData,
         { withCredentials: true },
       )
+      setNotification(
+        ` ${titleCase(productData.name, "_")} was added to your products list `,
+      )
+      setTimeout(() => setNotification(null), 1300)
     } catch (error) {
-      console.log("productData ", productData)
+      setNotification(`Error by creating new product`)
+      setTimeout(() => setNotification(null), 1300)
       console.error("Product submit failed: ", error)
     }
   }
 
   return (
-    <div className=''>
-      <div className={style.adminCrudContainer}>
-        <div className={style.pageTitle}>
+    <div className={styles.adminPageWrapper}>
+      {notification && <NotificationCard message={notification} />}
+      <div className={styles.adminPage}>
+        <div className={styles.adminFormTitel}>
           {adminData
             ? `Hello, ${adminData.firstName} ${adminData.lastName} you are loggin as a ${adminData.role}!`
             : "Loading admin data..."}
         </div>
-        <div>Admin Info Page</div>
-        <div className={style.productCrudContainer}>
-          <form
-            className={style.createProductForm}
-            onSubmit={submitProductForm}
-          >
-            <label>Name</label>
-            <input type='text' name='name' onChange={listenInputChange} />
-
-            <label>Category</label>
+        <div className={styles.containerTitle}>Admin Info Page</div>
+        <div className={styles.productCrudContainer}>
+          <form className={styles.form} onSubmit={submitProductForm}>
+            <label className={styles.label} htmlFor='name'>
+              Name
+            </label>
             <input
+              className={styles.formField}
+              type='text'
+              name='name'
+              onChange={listenInputChange}
+              required
+            />
+
+            <label className={styles.label} htmlFor='name'>
+              Category
+            </label>
+            <input
+              className={styles.formField}
               type='number'
               name='productCategoryId'
               onChange={listenInputChange}
+              required
             />
 
-            <label>Price</label>
-            <input type='number' name='price' onChange={listenInputChange} />
-
-            <label>Description</label>
+            <label className={styles.label} htmlFor='name'>
+              Price
+            </label>
             <input
+              className={styles.formField}
+              type='number'
+              name='price'
+              onChange={listenInputChange}
+              required
+            />
+
+            <label className={styles.label} htmlFor='description'>
+              Description
+            </label>
+            <input
+              className={styles.formField}
               type='text'
               name='description'
               onChange={listenInputChange}
+              required
             />
 
-            <label>Details</label>
-            <input type='text' name='details' onChange={listenInputChange} />
+            <label className={styles.label} htmlFor='details'>
+              Details
+            </label>
+            <input
+              className={styles.formField}
+              type='text'
+              name='details'
+              onChange={listenInputChange}
+              required
+            />
 
-            <label>Size</label>
-            <input type='number' name='size' onChange={listenInputChange} />
+            <label className={styles.label} htmlFor='size'>
+              Size
+            </label>
+            <input
+              className={styles.formField}
+              type='number'
+              name='size'
+              onChange={listenInputChange}
+              required
+            />
 
-            <label>Measure</label>
-            <input type='text' name='measure' onChange={listenInputChange} />
+            <label className={styles.label} htmlFor='measure'>
+              Measure
+            </label>
+            <input
+              className={styles.formField}
+              type='text'
+              name='measure'
+              onChange={listenInputChange}
+              required
+            />
 
-            <label>Image Upload</label>
-            <input type='text' name='image' onChange={listenInputChange} />
+            <label className={styles.label} htmlFor='image'>
+              Image Upload
+            </label>
+            <input
+              className={styles.formField}
+              type='text'
+              name='image'
+              onChange={listenInputChange}
+              required
+            />
 
-            <button type='submit'>submit</button>
+            <button className={styles.formButton} type='submit'>
+              submit
+            </button>
           </form>
         </div>
       </div>
