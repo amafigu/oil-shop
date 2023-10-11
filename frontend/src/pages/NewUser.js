21ee365dd2695161317cedc10580555524de1f64
Amadeus Figueiredo <amadeusfigue@gmail.com>
2023-10-11 09:10:27 +0200

    style new user page

--- frontend/src/pages/NewUser.js
+++ frontend/src/pages/NewUser.js
@@ -3,7 +3,7 @@ import { useEffectScrollTop } from "#utils/utils"
 import axios from "axios"
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
-import style from "./newUser.module.scss"
+import styles from "./newUser.module.scss"
 
 const NewUser = () => {
   const [firstName, setFirstName] = useState("")
@@ -18,58 +18,68 @@ const NewUser = () => {
   const createUser = async (e) => {
     e.preventDefault()
     try {
-      await axios.post(
-        `${process.env.REACT_APP_API_URL}/users/register-admin`,
-        {
-          firstName,
-          lastName,
-          email,
-          password,
-        },
-      )
+      await axios.post(`${process.env.REACT_APP_API_URL}/users/create`, {
+        firstName,
+        lastName,
+        email,
+        password,
+      })
 
-      navigate("/users/current-admin")
+      navigate("/users/current-user")
     } catch (error) {
       console.error("Signup error", error)
     }
   }
 
   return (
-    <div className=''>
-      <div className=''>
-        <div className={style.pageTitle}>
+    <div className={styles.newUserPageWrapper}>
+      <div className={styles.newUserPage}>
+        <div className={styles.containerTitle}>
           {text.title}
-          <form onSubmit={createUser}>
+          <form className={styles.form} onSubmit={createUser}>
+            <label className={styles.label} htmlFor='name'></label>
             <input
+              className={styles.formField}
+              name='firstName'
               type='text'
               value={firstName}
               placeholder={"first name"}
               onChange={(e) => setFirstName(e.target.value)}
               autoComplete='true'
+              required
             ></input>
             <input
+              className={styles.formField}
+              name='lastName'
               type='text'
               value={lastName}
               placeholder={"last name"}
               onChange={(e) => setLastName(e.target.value)}
               autoComplete='true'
+              required
             ></input>
             <input
+              className={styles.formField}
               type='email'
               value={email}
               placeholder={text.emailPlaceholder}
               onChange={(e) => setEmail(e.target.value)}
               autoComplete='true'
+              required
             ></input>
             <input
+              className={styles.formField}
               type='password'
               value={password}
               placeholder={text.passwordPlaceholder}
               onChange={(e) => setPassword(e.target.value)}
               autoComplete='true'
+              required
             ></input>
 
-            <button type='submit'>Sign Up</button>
+            <button className={styles.formButton} type='submit'>
+              Sign Up
+            </button>
           </form>
         </div>
         <div></div>
