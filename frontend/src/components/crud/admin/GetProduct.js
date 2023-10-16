1df28a6cee9153d30981a8ceaff5ee2390c3ad48
Amadeus Figueiredo <amadeusfigue@gmail.com>
2023-10-17 00:33:18 +0200

    fix-lint

--- frontend/src/components/crud/admin/GetProduct.js
+++ frontend/src/components/crud/admin/GetProduct.js
@@ -1,19 +1,11 @@
-import NotificationCard from "#components/NotificationCard"
-import useLocaleContext from "#context/localeContext"
 import { useState } from "react"
 import styles from "./getProduct.module.scss"
 
 const GetProduct = () => {
   const [userEmail, setUserEmail] = useState("")
-  const [userDataByEmail, setUserDataByEmail] = useState({})
-  const [notification, setNotification] = useState(null)
-
-  const { translate } = useLocaleContext()
-  const text = translate.components.crud
 
   return (
     <div>
-      {notification && <NotificationCard message={notification} />}
       <input
         type='text'
         value={userEmail}
@@ -21,22 +13,6 @@ const GetProduct = () => {
         onChange={(e) => setUserEmail(e.target.value)}
       />
       <button className={styles.formButton}></button>
-      {/*  <div>
-        {text.forms.commonProperties.firstName}:{" "}
-        {userDataByEmail ? userDataByEmail.firstName : ""}
-      </div>
-      <div>
-        {text.forms.commonProperties.lastName}:
-        {userDataByEmail ? userDataByEmail.lastName : ""}
-      </div>
-      <div>
-        {text.forms.commonProperties.role}:
-        {userDataByEmail ? userDataByEmail.role : ""}
-      </div>
-      <div>
-        {text.forms.commonProperties.createdAt}:
-        {userDataByEmail ? userDataByEmail.createdAt : ""}
-      </div>*/}
     </div>
   )
 }
