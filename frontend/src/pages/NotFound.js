7e9789af94e0c0fdf3272b5285196ec2122683bd
Amadeus Figueiredo <amadeusfigue@gmail.com>
2023-10-31 14:48:32 +0100

    provisory page

---
+++ frontend/src/pages/NotFound.js
@@ -0,0 +1,21 @@
+import styles from "./notFound.module.scss"
+
+const NotFound = () => {
+  return (
+    <div className={styles.notFoundPageWrapper}>
+      <div className={styles.notFoundPage}>
+        \
+        <div className={styles.container}>
+          <img
+            className={styles.logo}
+            src={`${process.env.PUBLIC_URL}/assets/logo.png`}
+            alt='logo'
+          />
+          <div className={styles.message}>Sorry, this page is not there</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default NotFound
