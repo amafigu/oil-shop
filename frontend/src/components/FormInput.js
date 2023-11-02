9a6846ef0f12e30a2f825ef38fe68103f1a991a9
Amadeus Figueiredo <amadeusfigue@gmail.com>
2023-11-02 12:24:48 +0100

    form input component

---
+++ frontend/src/components/FormInput.js
@@ -0,0 +1,30 @@
+import styles from "./formInput.module.scss"
+
+const FormInput = ({
+  classCss,
+  name,
+  value,
+  onChangeListener,
+  placeholder,
+  label,
+  type,
+}) => {
+  return (
+    <div className={styles.inputContainer}>
+      <label className={styles.label} htmlFor={name}>
+        {label}
+      </label>
+      <input
+        className={`${styles}.${classCss}`}
+        type={type}
+        name={name}
+        onChange={onChangeListener}
+        value={value}
+        placeholder={placeholder}
+        id={name}
+      />
+    </div>
+  )
+}
+
+export default FormInput
