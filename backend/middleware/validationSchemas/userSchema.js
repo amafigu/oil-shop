e54f20fa7358d23e26f8d930d09d2e31c568d96c
Amadeus Figueiredo <amadeusfigue@gmail.com>
2023-12-04 15:12:18 +0100

    shipping data validation and minor changes

--- backend/middleware/validationSchemas/userSchema.js
+++ backend/middleware/validationSchemas/userSchema.js
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 
-const firstNameValidation = z
+const firstAndLastNameValidation = z
   .string()
   .min(2, 'First name should have at least two characters.')
   .max(30, "Can't be longer than 30 characters.")
@@ -11,45 +11,71 @@ const firstNameValidation = z
     message: 'First name can only contain letters.',
   });
 
-const lastNameValidation = z
-  .string()
-  .min(2, 'Last name should have at least two characters.')
-  .max(30, "Can't be longer than 40 characters.")
-  .refine((value) => value[0] === value[0].toUpperCase(), {
-    message: 'Last name should start with a capital letter.',
-  })
-  .refine((value) => /^[A-Za-z\s]+$/.test(value), {
-    message: 'Last name can only contain letters.',
-  });
-const emailValidation = z
-  .string()
-  .min(4, 'Email should have at least 4 characters.')
-  .email('Invalid email format.')
-  .max(40, "Email can't be longer than 40 characters.");
-
-const passwordValidation = z
-  .string()
-  .min(8, 'Password should have at least 8 characters.')
-  .refine((value) => /[A-Z]/.test(value), {
-    message: 'Password should have at least one uppercase character.',
-  })
-  .refine((value) => /[!#$%&()*+,-./:;<=>?@[\\\]^_`{|}~]/.test(value), {
-    message: 'Password should have at least one symbol.',
-  });
-
-const CreateUserSchema = z.object({
-  firstName: firstNameValidation,
-  lastName: lastNameValidation,
-  email: emailValidation,
-  password: passwordValidation,
+const createUserValidation = z.object({
+  firstName: firstAndLastNameValidation
+    .refine((value) => value[0] === value[0].toUpperCase(), {
+      message: 'First name should start with a capital letter.',
+    })
+    .refine((value) => /^[A-Za-z\s]+$/.test(value), {
+      message: 'First name can only contain letters.',
+    }),
+  lastName: firstAndLastNameValidation
+    .refine((value) => value[0] === value[0].toUpperCase(), {
+      message: 'Last name should start with a capital letter.',
+    })
+    .refine((value) => /^[A-Za-z\s]+$/.test(value), {
+      message: 'Last name can only contain letters.',
+    }),
+  email: z
+    .string()
+    .min(4, 'Email should have at least 4 characters.')
+    .email('Invalid email format. ')
+    .max(40, "Email can't be longer than 40 characters."),
+  password: z
+    .string()
+    .min(8, 'Password should have at least 8 characters.')
+    .refine((value) => /[A-Z]/.test(value), {
+      message: 'Password should have at least one uppercase character.',
+    })
+    .refine((value) => /[!#$%&()*+,-./:;<=>?@[\\\]^_`{|}~]/.test(value), {
+      message: 'Password should have at least one symbol.',
+    }),
   image: z.string().optional(),
   createdAt: z.string().or(z.date()).optional(),
   updatedAt: z.string().or(z.date()).optional(),
 });
 
-const LoginSchema = z.object({
-  email: emailValidation,
+const shippingDataValidation = z.object({
+  street: z
+    .string()
+    .max(60, "Can't be longer than 30 characters.")
+    .optional()
+    .default(''),
+  number: z
+    .string()
+    .max(10, "Can't be longer than 10 characters.")
+    .optional()
+    .default(''),
+  details: z.string().max(50, "Can't be longer than 50 characters.").optional(),
+  postalCode: z
+    .string()
+    .max(10, "Can't be longer than 10 characters.")
+    .optional()
+    .default(''),
+  city: z
+    .string()
+    .max(30, "Can't be longer than 30 characters.")
+    .optional()
+    .default(''),
+});
+
+const loginValidation = z.object({
+  email: z
+    .string()
+    .min(4, 'Email should have at least 4 characters.')
+    .email('Invalid email format. ')
+    .max(40, "Email can't be longer than 40 characters."),
   password: z.string().optional(),
 });
 
-export { CreateUserSchema, LoginSchema };
+export { createUserValidation, loginValidation, shippingDataValidation };
