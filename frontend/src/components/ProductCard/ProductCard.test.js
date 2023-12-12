203f24634022e42930432f3bf23d75d8f74e9ef1
Amadeus Figueiredo <amadeusfigue@gmail.com>
2023-12-12 15:06:53 +0100

    product card attributes values rendering test

--- frontend/src/components/ProductCard/ProductCard.test.js
+++ frontend/src/components/ProductCard/ProductCard.test.js
@@ -1,25 +1,26 @@
-/*import useCartContext from "#context/cartContext"
+import useCartContext from "#context/cartContext"
 import useLocaleContext from "#context/localeContext"
 import "@testing-library/jest-dom"
 import { render } from "@testing-library/react"
+import React from "react"
 import { MemoryRouter } from "react-router-dom"
 import ProductCard from "./index"
 
 jest.mock("#context/localeContext")
 jest.mock("#context/cartContext")
 
-test("renders name correctly", () => {
-  const product = {
-    name: "Test Product",
-    image: "image.jpg",
-    size: "15",
-    price: "25",
-    description: "description",
-    category: "massageOil",
-  }
+describe("ProductCard should ", () => {
+  test("renders properties value correctly", () => {
+    const product = {
+      name: "Test Product",
+      image: "image.jpg",
+      size: "15",
+      price: "25",
+      description: "description",
+      category: "massageOil",
+    }
 
-  useLocaleContext.mockReturnValue({
-    translate: {
+    const translate = {
       components: {
         products: {
           oil: { size: "size", price: "price" },
@@ -28,23 +29,33 @@ test("renders name correctly", () => {
           text: "Add to cart",
         },
       },
-    },
+    }
+
+    useLocaleContext.mockReturnValue({ translate })
+
+    useCartContext.mockReturnValue({
+      addProduct: jest.fn(),
+      updateProductQuantity: jest.fn(),
+      removeProduct: jest.fn(),
+      getAllProductsQuantity: 0,
+      cart: [],
+    })
+
+    const { getByText } = render(
+      <MemoryRouter>
+        <ProductCard product={product} />
+      </MemoryRouter>,
+    )
+
+    expect(getByText(product.name)).toBeInTheDocument()
+    expect(
+      getByText(`${translate.components.products.oil.price} €${product.price}`),
+    ).toBeInTheDocument()
+    expect(getByText(product.description)).toBeInTheDocument()
+    expect(
+      getByText(
+        `${translate.components.products.oil.size}: ${product.size} ml`,
+      ),
+    ).toBeInTheDocument()
   })
-
-  useCartContext.mockReturnValue({
-    addProduct: jest.fn(),
-    updateProductQuantity: jest.fn(),
-    removeProduct: jest.fn(),
-    getAllProductsQuantity: 0,
-    cart: [],
-  })
-
-  const { getByText } = render(
-    <MemoryRouter>
-      <ProductCard {...product} />
-    </MemoryRouter>,
-  )
-
-  expect(getByText(product.name)).toBeInTheDocument()
 })
-*/
