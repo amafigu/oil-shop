4b3b2ffc00c9a293cfbe3450aaea42f45190cb61
Amadeus Figueiredo <amadeusfigue@gmail.com>
2023-12-12 12:27:43 +0100

    add and remove products from cart

--- frontend/src/pages/Cart.test.js
+++ frontend/src/pages/Cart.test.js
@@ -2,89 +2,97 @@ import useCartContext from "#context/cartContext"
 import useLocaleContext from "#context/localeContext"
 import "@testing-library/jest-dom"
 import { fireEvent, render } from "@testing-library/react"
+import React from "react"
 import { MemoryRouter } from "react-router-dom"
 import Cart from "./Cart"
 
 jest.mock("#context/cartContext")
 jest.mock("#context/localeContext")
 
-test("renders products correctly", () => {
-  useCartContext.mockReturnValue({
-    cart: [
-      {
-        product: {
-          name: "Test Product",
-          image: "image.jpg",
-          description: "Description",
-          size: 100,
-          price: 10,
-        },
-        quantity: 2,
-      },
-    ],
-    removeProduct: jest.fn(),
-    updateProductQuantity: jest.fn(),
-  })
-
-  useLocaleContext.mockReturnValue({
-    translate: {
-      pages: {
-        cart: {
-          deleteButton: "Delete",
-        },
-      },
-    },
-  })
-
-  const { getByText } = render(
-    <MemoryRouter>
-      <Cart />
-    </MemoryRouter>,
-  )
-  expect(getByText("Test Product")).toBeInTheDocument()
+beforeAll(() => {
+  window.scrollTo = jest.fn()
 })
 
-test("removes product correctly", () => {
-  const mockRemoveProduct = jest.fn()
-
-  useCartContext.mockReturnValue({
-    cart: [
-      {
-        product: {
-          name: "Test Product",
-          image: "image.jpg",
-          description: "Description",
-          size: 100,
-          price: 10,
+describe("Cart should ", () => {
+  test("renders products correctly", () => {
+    useCartContext.mockReturnValue({
+      cart: [
+        {
+          product: {
+            name: "Test Product",
+            image: "image.jpg",
+            description: "Description",
+            size: 100,
+            price: 10,
+          },
+          quantity: 2,
         },
-        quantity: 2,
-      },
-    ],
-    removeProduct: mockRemoveProduct,
-    updateProductQuantity: jest.fn(),
-  })
+      ],
+      removeProduct: jest.fn(),
+      updateProductQuantity: jest.fn(),
+    })
 
-  useLocaleContext.mockReturnValue({
-    translate: {
-      pages: {
-        cart: {
-          deleteButton: "Delete",
-          orderSummary: "Order Summary",
-          orderSubtotal: "Order Subtotal",
-          orderShipping: "Shipping",
-          orderTotal: "Total",
+    // use i18n of translate text object
+    useLocaleContext.mockReturnValue({
+      translate: {
+        pages: {
+          cart: {
+            deleteButton: "Delete",
+          },
         },
       },
-    },
+    })
+
+    const { getByText } = render(
+      <MemoryRouter>
+        <Cart />
+      </MemoryRouter>,
+    )
+    expect(getByText("Test Product")).toBeInTheDocument()
   })
 
-  const { getByText } = render(
-    <MemoryRouter>
-      <Cart />
-    </MemoryRouter>,
-  )
+  test("removes product correctly", () => {
+    const mockRemoveProduct = jest.fn()
 
-  fireEvent.click(getByText("Delete"))
+    useCartContext.mockReturnValue({
+      cart: [
+        {
+          product: {
+            name: "Test Product",
+            image: "image.jpg",
+            description: "Description",
+            size: 100,
+            price: 10,
+          },
+          quantity: 2,
+        },
+      ],
+      removeProduct: mockRemoveProduct,
+      updateProductQuantity: jest.fn(),
+    })
+    // use i18n of translate text object
+    useLocaleContext.mockReturnValue({
+      translate: {
+        pages: {
+          cart: {
+            deleteButton: "Delete",
+            orderSummary: "Order Summary",
+            orderSubtotal: "Order Subtotal",
+            orderShipping: "Shipping",
+            orderTotal: "Total",
+          },
+        },
+      },
+    })
 
-  expect(mockRemoveProduct).toHaveBeenCalledWith("Test Product")
+    const { getByText } = render(
+      <MemoryRouter>
+        <Cart />
+      </MemoryRouter>,
+    )
+
+    fireEvent.click(getByText("Delete"))
+
+    expect(mockRemoveProduct).toHaveBeenCalledWith("Test Product")
+  })
 })
