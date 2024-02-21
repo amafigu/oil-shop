a8399c77d2408c2282ad878229b7dc31de57068f
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-02-21 12:30:39 +0100

    custom hook for product details

---
+++ frontend/src/hooks/useProductDetails.js
@@ -0,0 +1,32 @@
+import useLocaleContext from "#context/localeContext"
+import { LONG_MESSAGE_TIMEOUT, ROUTES_SHOP } from "#utils/constants"
+import { getProductByName } from "#utils/products"
+import { useEffect, useState } from "react"
+import { useNavigate, useParams } from "react-router-dom"
+
+export const useProductDetails = () => {
+  const [product, setProduct] = useState(null)
+  const [notification, setNotification] = useState(null)
+  const { translate } = useLocaleContext()
+  const text = translate.pages.productsDetails
+  const navigate = useNavigate()
+  const { productName } = useParams()
+
+  useEffect(() => {
+    const getProductDetails = async () => {
+      try {
+        const response = await getProductByName(productName)
+        if (response.status === 200) {
+          setProduct(response.data)
+        }
+      } catch (error) {
+        setNotification(text.errorByGettingProduct)
+        setTimeout(() => navigate(ROUTES_SHOP), LONG_MESSAGE_TIMEOUT)
+        setTimeout(() => setNotification(null), LONG_MESSAGE_TIMEOUT)
+      }
+    }
+    getProductDetails()
+  }, [productName, text.errorByGettingProduct, navigate])
+
+  return { product, notification }
+}
