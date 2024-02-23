ced1cfca6e59b36213266180c2b15c32b25ddd6a
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-02-23 14:34:57 +0100

    extract TeaserCard and TeaserText from Home and clean scss files

---
+++ frontend/src/pages/Home/TeaserCard.js
@@ -0,0 +1,23 @@
+import { useTranslation } from "#hooks/useTranslation"
+import { getTeaserCardIconByName } from "#utils/icons"
+import { accessTranslationWithPathString } from "#utils/translation"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import styles from "./teaserCard.module.scss"
+
+export const TeaserCard = ({ title, text, iconName }) => {
+  const translate = useTranslation()
+
+  return (
+    <div className={styles.teaserSingleCard}>
+      <p className={styles.teaserCardTitle}>
+        {accessTranslationWithPathString(translate, title)}
+      </p>
+      <p className={styles.teaserCardText}>
+        {accessTranslationWithPathString(translate, text)}
+      </p>
+      <span className={styles.iconColumns}>
+        <FontAwesomeIcon icon={getTeaserCardIconByName(iconName)} size='xs' />
+      </span>
+    </div>
+  )
+}
