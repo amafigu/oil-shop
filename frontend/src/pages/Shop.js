88efe19e93ab3bfbc66e75a898248c460bf9c61d
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-02-26 00:58:45 +0100

    refactoring: extract product list and use named import

--- frontend/src/pages/Shop.js
+++ frontend/src/pages/Shop.js
@@ -1,46 +1,27 @@
-import ProductCard from "#components/products/ProductCard"
+import { SortedProductsList } from "#components/products/SortedProductsList"
 import Sidebar from "#components/ui/Sidebar"
 import { useProductCategory } from "#hooks/useProductCategory"
-import { useGetProducts } from "#utils/products"
 import { scrollToTop } from "#utils/render"
-import React, { useState } from "react"
-import style from "./shop.module.scss"
+import React from "react"
+import styles from "./shop.module.scss"
 
-const Shop = ({ productCategories }) => {
-  const [products, setProducts] = useState([])
+export const Shop = ({ productCategories }) => {
   const { category, setCategory } = useProductCategory()
   scrollToTop()
-  useGetProducts(setProducts)
-
-  const filteredProducts = (category) =>
-    products.filter(
-      (product) => product.category.name === category || category === "all",
-    )
-
-  const sortedProducts = filteredProducts(category)
 
   return (
-    <>
-      <div className={style.shopPageWrapper}>
-        <div className={style.shopPage}>
-          <div className={style.sidebarWrapper}>
-            <Sidebar
-              setCategory={setCategory}
-              productCategories={productCategories}
-            />
-          </div>
-
-          <div className={style.sortedProducts}>
-            {sortedProducts.map((product, index) => (
-              <div key={index}>
-                <ProductCard product={product} />
-              </div>
-            ))}
-          </div>
-        </div>
+    <main className={styles.shopPageWrapper}>
+      <div className={styles.shopPage}>
+        <nav className={styles.sidebarWrapper}>
+          <Sidebar
+            setCategory={setCategory}
+            productCategories={productCategories}
+          />
+        </nav>
+        <section className={styles.sortedProductsSection}>
+          <SortedProductsList category={category} />
+        </section>
       </div>
-    </>
+    </main>
   )
 }
-
-export default Shop
