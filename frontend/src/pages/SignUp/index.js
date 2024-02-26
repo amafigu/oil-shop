e571f95ff239af2df675de2468c7fafc71547caa
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-02-26 17:46:29 +0100

    extract content and improve accesibility

---
+++ frontend/src/pages/SignUp/index.js
@@ -0,0 +1,31 @@
+import SubNavbar from "#components/ui/Navbar/SubNavbar"
+import CreateUserForm from "#components/users/UsersCrud/CreateUserForm"
+import { LOGO_IMAGE } from "#constants/media"
+import { scrollToTop } from "#utils/render"
+import { LinkContainer } from "./LinkContainer"
+import styles from "./signUp.module.scss"
+
+export const SignUp = () => {
+  scrollToTop()
+
+  return (
+    <main className={styles.signUpPage}>
+      <div className={styles.subNavbarContainer}>
+        <SubNavbar />
+      </div>
+      <section className={styles.logoAndSignUpForm}>
+        <div className={styles.logoContainer}>
+          <img
+            className={styles.logo}
+            src={`${process.env.PUBLIC_URL}${LOGO_IMAGE}`}
+            alt='logo'
+          />
+        </div>
+        <div className={styles.form}>
+          <CreateUserForm />
+        </div>
+        <LinkContainer />
+      </section>
+    </main>
+  )
+}
