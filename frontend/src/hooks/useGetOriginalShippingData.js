584b3ea3448d478666d3a537b1720c8d4cb56294
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-02-27 12:48:28 +0100

    extract originalShippingData into hook

---
+++ frontend/src/hooks/useGetOriginalShippingData.js
@@ -0,0 +1,41 @@
+import { API_SHIPPING_DATA } from "#constants/api"
+import useUserContext from "#context/userContext"
+import { getDataAndSetErrorMessage } from "#utils/dataManipulation"
+import { useEffect, useState } from "react"
+
+export const useGetOriginalShippingData = () => {
+  const { userId, isLoading } = useUserContext()
+  const initialShippingData = {
+    street: "",
+    number: "",
+    details: "",
+    postalCode: "",
+    city: "",
+    state: "",
+    country: "",
+  }
+  const [nonUpdatedShippingData, setNonUpdatedShippingData] = useState({
+    ...initialShippingData,
+  })
+
+  useEffect(() => {
+    async function getOriginalShippingData() {
+      if (!isLoading) {
+        const shippingData = await getDataAndSetErrorMessage(
+          userId,
+          API_SHIPPING_DATA,
+        )
+        if (!shippingData) {
+          return
+        }
+        if (shippingData.status === 200) {
+          console.log(shippingData.data)
+          setNonUpdatedShippingData(shippingData.data)
+        }
+      }
+    }
+    getOriginalShippingData()
+  }, [userId, isLoading])
+
+  return { nonUpdatedShippingData, setNonUpdatedShippingData, userId }
+}
