1cf4ddb393a71d3a95459e5b811fee85804568a3
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-03-01 13:52:21 +0100

    add prop to useTranslation

--- frontend/src/hooks/useTranslation.js
+++ frontend/src/hooks/useTranslation.js
@@ -3,5 +3,6 @@ import useLocaleContext from "#context/localeContext"
 export const useTranslation = () => {
   const { translate, language, setLanguage } = useLocaleContext()
   const commonProperties = translate.commonProperties
-  return { translate, language, setLanguage, commonProperties }
+  const components = translate.components
+  return { translate, language, setLanguage, commonProperties, components }
 }
