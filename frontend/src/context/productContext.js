24dc30283c3cf7642c59ccf4429b0eaee186bf65
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-03-02 01:08:18 +0100

    add product context

---
+++ frontend/src/context/productContext.js
@@ -0,0 +1,34 @@
+import { getAllProductsList } from "#api/products/getAllProductsList"
+import { createContext, useContext, useEffect, useState } from "react"
+
+export const ProductContext = createContext()
+
+export const ProductProvider = ({ children }) => {
+  const [products, setProducts] = useState([])
+  const [counter, setCounter] = useState(0)
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const response = await getAllProductsList()
+        if (response) {
+          setProducts(response)
+        }
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    getProducts()
+  }, [])
+
+  return (
+    <ProductContext.Provider
+      value={{ counter, setCounter, products, setProducts }}
+    >
+      {children}
+    </ProductContext.Provider>
+  )
+}
+
+const useProductContext = () => useContext(ProductContext)
+export default useProductContext
