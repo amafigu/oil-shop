8c06b29657875cbdb4285470b91d11befe1c1e75
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-03-04 19:01:04 +0100

    add properties

---
+++ frontend/src/constants/users.js
@@ -0,0 +1,15 @@
+export const emptyCreateUserObj = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+  image: "",
+}
+
+export const initialUserData = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+  image: "",
+}
