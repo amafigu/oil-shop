4fe94e3e8e79c95322638e190b18063f714da45d
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-03-05 16:18:19 +0100

    adapt for images

--- frontend/src/components/ui/FormInput.js
+++ frontend/src/components/ui/FormInput.js
@@ -9,23 +9,44 @@ export const FormInput = ({
   onChangeListener,
   placeholder,
   label,
-  type,
+  type = "text",
+  file,
 }) => {
   return (
     <div className={styles.inputContainer} aria-label={`${name} input`}>
-      <label className={styles.label} htmlFor={name}>
-        {label}
-      </label>
-      <input
-        className={styles[classCss]}
-        type={type}
-        name={name}
-        onChange={onChangeListener}
-        value={value}
-        placeholder={camelCaseToTitleCase(placeholder)}
-        required
-        autoComplete='true'
-      />
+      {name === "image" ? (
+        <>
+          <span className={styles.label}>
+            {file ? "Selected file: " : "Select a file"}
+          </span>
+          <label className={styles.labelForFile} htmlFor='fileInput'>
+            {file ? file.name : "Search on device"}
+          </label>
+          <input
+            type='file'
+            name='image'
+            id='fileInput'
+            onChange={onChangeListener}
+            required
+          />
+        </>
+      ) : (
+        <>
+          <label className={styles.label} htmlFor={name}>
+            {label}
+          </label>
+          <input
+            className={styles[classCss]}
+            type={type}
+            name={name}
+            onChange={onChangeListener}
+            value={value}
+            placeholder={camelCaseToTitleCase(placeholder)}
+            required
+            autoComplete='true'
+          />
+        </>
+      )}
     </div>
   )
 }
