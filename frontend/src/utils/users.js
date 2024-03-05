8e506ca41b144183cc620eab4fe3471c661136a1
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-03-05 12:21:31 +0100

    add update user handler

--- frontend/src/utils/users.js
+++ frontend/src/utils/users.js
@@ -1,6 +1,9 @@
+import { uploadToS3 } from "#api/aws/uploadToS3"
 import { deleteUserById } from "#api/users/deleteUserById"
+import { updateUserDataRequest } from "#api/users/updateUserDataRequest"
 import { ROUTES_CART, ROUTES_CHECKOUT_PAYMENT } from "#constants/routes"
-import { NORMAL_MESSAGE_TIMEOUT } from "#constants/time"
+import { NORMAL_MESSAGE_TIMEOUT, SHORT_MESSAGE_TIMEOUT } from "#constants/time"
+import { validateUserProperties } from "#utils/validation"
 export const submitGuestUserWithOrders = async (
   e,
   cart,
@@ -49,6 +52,53 @@ export const onDeleteUser = async (e, userId, setNotification, setCounter) => {
   }
 }
 
-export const onUpdateUser = () => {
-  console.log("onUpdateUser")
+export const onUpdateUser = async (
+  e,
+  key,
+  userId,
+  updatedUserData,
+  nonUpdatedUserData,
+  setUpdatedUserData,
+  setNotification,
+  file,
+) => {
+  e.preventDefault()
+  try {
+    let validProperty
+    let image
+
+    if (key === "image" && file) {
+      image = await uploadToS3(file)
+      validProperty = { [key]: image }
+    } else {
+      const toBevalidProperty = { [key]: updatedUserData[key] }
+      validProperty = validateUserProperties(toBevalidProperty, setNotification)
+    }
+    if (!validProperty) {
+      setUpdatedUserData(updatedUserData)
+      return
+    }
+
+    const dataRequest = await updateUserDataRequest(userId, validProperty)
+    if (dataRequest && dataRequest.status === 200) {
+      const updatedUser = dataRequest.data.user
+      setTimeout(() => setUpdatedUserData(updatedUser), 500)
+      return updatedUser
+    }
+  } catch (error) {
+    setUpdatedUserData(nonUpdatedUserData)
+    if (error.response && error.response.data.errors) {
+      if (setNotification) {
+        setNotification(
+          `Error by updating data: ${error.response.data.errors[0].message}`,
+        )
+        setTimeout(() => setNotification(null), SHORT_MESSAGE_TIMEOUT)
+      }
+    } else {
+      if (setNotification) {
+        setNotification("Error by updating data")
+        setTimeout(() => setNotification(null), SHORT_MESSAGE_TIMEOUT)
+      }
+    }
+  }
 }
