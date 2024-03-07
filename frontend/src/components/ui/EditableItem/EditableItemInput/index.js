f2877f29aef9c4d5d7063992ac39d8b68dda9c80
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-03-07 11:29:19 +0100

    clean log and commented code

--- frontend/src/components/ui/EditableItem/EditableItemInput/index.js
+++ frontend/src/components/ui/EditableItem/EditableItemInput/index.js
@@ -15,9 +15,10 @@ export const EditableItemInput = ({
   classCss,
   file,
 }) => {
+  debugger
   const [isEditing, setIsEditing] = useState(false)
   const { commonProperties, commonButtons } = useTranslation()
-
+  console.log(updatedPropertyData)
   const saveEdition = (e) => {
     onSave(e)
     setIsEditing(false)
@@ -52,7 +53,7 @@ export const EditableItemInput = ({
       ) : isEditing ? (
         <>
           <label htmlFor={name} className={styles.hideForSemantic}>
-            {commonProperties[name]}
+            {/* commonProperties[name] || "" */}
           </label>
           <input
             aria-label={`${label} input`}
