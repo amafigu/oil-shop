4d0f98ca6decfccb0c0306fd3e76dff2a8df1df9
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-03-07 11:30:38 +0100

    refactor user

--- frontend/src/pages/User/index.js
+++ frontend/src/pages/User/index.js
@@ -6,36 +6,33 @@ import { editableUserProperties } from "#constants/users"
 import { useCheckIsUser } from "#hooks/useCheckIsUser"
 import { useGetUserShippingData } from "#hooks/useGetUserShippingData"
 import { onUpdateUser, onUpdateUserShippingData } from "#utils/users"
-
 import styles from "./user.module.scss"
 export const User = () => {
   const { notification, user } = useCheckIsUser()
   const { shippingData } = useGetUserShippingData()
-  console.log(user)
-  console.log(shippingData)
 
   return (
     <main className={styles.userPage} aria-label='Customer Management Page'>
-      <div className={styles.userPage}>
-        {notification && <NotificationCard message={notification} />}
-        <section className={styles.container}>
-          <UserHeader />
-          {user && (
-            <EditableItem
-              item={user}
-              renderItemProps={editableUserProperties}
-              onSave={onUpdateUser}
-            />
-          )}
-          {shippingData && (
-            <EditableItem
-              item={shippingData}
-              renderItemProps={editableUserShippingDataProperties}
-              onSave={onUpdateUserShippingData}
-            />
-          )}
-        </section>
-      </div>
+      {notification && <NotificationCard message={notification} />}
+      <section className={styles.container}>
+        <UserHeader />
+
+        {user && (
+          <EditableItem
+            item={user}
+            renderItemProps={editableUserProperties}
+            onSave={onUpdateUser}
+          />
+        )}
+
+        {shippingData && shippingData.hasOwnProperty("id") && (
+          <EditableItem
+            item={shippingData}
+            renderItemProps={editableUserShippingDataProperties}
+            onSave={onUpdateUserShippingData}
+          />
+        )}
+      </section>
     </main>
   )
 }
