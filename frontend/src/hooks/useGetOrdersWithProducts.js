733337b1875b2c7814694b9d6c0b1f5a6a779f8c
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-03-07 13:49:30 +0100

    adjust orders hook

--- frontend/src/hooks/useGetOrdersWithProducts.js
+++ frontend/src/hooks/useGetOrdersWithProducts.js
@@ -1,48 +1,54 @@
-import { getUserOrdersWithProductsList } from "#api/orders/getUserOrdersWithProductsList"
-import useUserContext from "#context/userContext"
-import { useCallback, useRef, useState } from "react"
+import { getUserOrderItemsRequest } from "#api/orders/getUserOrderItemsRequest"
+import { getUserOrdersRequest } from "#api/orders/getUserOrdersRequest"
+import { useCheckIsUser } from "#hooks/useCheckIsUser"
+import { useEffect, useRef, useState } from "react"
 
 export const useGetOrdersWithProducts = () => {
   const [orders, setOrders] = useState([])
   const [notification, setNotification] = useState(null)
-  const { userId } = useUserContext()
+  const { user } = useCheckIsUser()
   const [showOrders, setShowOrders] = useState(false)
 
-  const makeOrderTimeoutId = useRef(null)
   const noGetOrdersTimeoutId = useRef(null)
 
-  const getOrdersWithProducts = useCallback(async () => {
-    try {
-      const ordersWithDetails = await getUserOrdersWithProductsList(userId)
-      setOrders(ordersWithDetails)
-      if (ordersWithDetails.length === 0) {
-        setShowOrders(false)
+  useEffect(() => {
+    const getOrdersWithProducts = async () => {
+      console.log(user)
+      if (!user) return
+
+      try {
+        const response = await getUserOrdersRequest(user.id)
+
+        if (response && response.status === 200) {
+          const ordersWithDetails = await Promise.all(
+            response.data.map(async (order) => {
+              const cartItemsResponse = await getUserOrderItemsRequest(order.id)
+              return { ...order, cartItems: cartItemsResponse.data }
+            }),
+          )
+
+          console.log(ordersWithDetails)
+
+          setOrders(ordersWithDetails)
+        }
+      } catch (error) {
+        console.error(error)
         setNotification(
-          "You have no orders yet, please make an order and come back later",
+          "It is not possible to get the orders at the moment, please try again",
         )
-        makeOrderTimeoutId.current = setTimeout(
+        noGetOrdersTimeoutId.current = setTimeout(
           () => setNotification(null),
-          2000,
+          3000,
         )
       }
-    } catch (error) {
-      console.error(error)
-      setNotification(
-        "It is not possible to get the orders at the moment, please try again",
-      )
-      noGetOrdersTimeoutId.current = setTimeout(
-        () => setNotification(null),
-        3000,
-      )
     }
+    getOrdersWithProducts()
     return () => {
-      clearTimeout(makeOrderTimeoutId.current)
       clearTimeout(noGetOrdersTimeoutId.current)
     }
-  }, [userId])
+  }, [user])
 
   return {
-    getOrdersWithProducts,
     showOrders,
     setShowOrders,
     orders,
