b9b0edef219cf36471eac131b267b4167f39bd58
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-03-07 13:45:02 +0100

    refactor orders functions

---
+++ frontend/src/api/orders/getUserOrderItemsRequest.js
@@ -0,0 +1,11 @@
+import { API_ORDERS_CART_ITEMS } from "#constants/api"
+import axios from "axios"
+
+export const getUserOrderItemsRequest = async (orderId) => {
+  const response = await axios.get(
+    `${process.env.REACT_APP_API_URL}${API_ORDERS_CART_ITEMS}/${orderId}`,
+
+    { withCredentials: true },
+  )
+  return response
+}
