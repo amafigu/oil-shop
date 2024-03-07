158fca290bda1c358d194473e91fd0971ce9450c
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-03-07 08:50:26 +0100

    conditional render

--- frontend/src/pages/User/index.js
+++ frontend/src/pages/User/index.js
@@ -1,15 +1,17 @@
+import { EditableItem } from "#components/ui/EditableItem"
 import NotificationCard from "#components/ui/NotificationCard"
 import { UserHeader } from "#components/ui/UserHeader"
 import { editableUserShippingDataProperties } from "#constants/shippingData"
+import { editableUserProperties } from "#constants/users"
 import { useCheckIsUser } from "#hooks/useCheckIsUser"
 import { useGetUserShippingData } from "#hooks/useGetUserShippingData"
-import { EditableItem } from "#pages/Admin/EditableItemsList/EditableItem"
-import { onUpdateUserShippingData } from "#utils/users"
+import { onUpdateUser, onUpdateUserShippingData } from "#utils/users"
 
 import styles from "./user.module.scss"
 export const User = () => {
   const { notification, user } = useCheckIsUser()
   const { shippingData } = useGetUserShippingData()
+  console.log(user)
   console.log(shippingData)
 
   return (
@@ -18,19 +20,20 @@ export const User = () => {
         {notification && <NotificationCard message={notification} />}
         <section className={styles.container}>
           <UserHeader />
-          {/* <EditableItem
-            item={user}
-            renderItemProps={editableUserProperties}
-            onSave={onUpdateUser}
-          />*/}
-
-          {
+          {user && (
+            <EditableItem
+              item={user}
+              renderItemProps={editableUserProperties}
+              onSave={onUpdateUser}
+            />
+          )}
+          {shippingData && (
             <EditableItem
               item={shippingData}
               renderItemProps={editableUserShippingDataProperties}
               onSave={onUpdateUserShippingData}
             />
-          }
+          )}
         </section>
       </div>
     </main>
