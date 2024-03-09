a9d7277586a5766ac0a22a87678550a5deedcdc5
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-03-10 00:01:50 +0100

    add and refactor categories hooks

--- frontend/src/hooks/useGetProductCategories.js
+++ frontend/src/hooks/useGetProductCategories.js
@@ -2,18 +2,23 @@ import { getProductCategories } from "#api/products/getProductCategories"
 import { useEffect, useState } from "react"
 
 export const useGetProductCategories = () => {
+  const [notification, setNotification] = useState(null)
   const [productCategories, setProductCategories] = useState("")
+
   useEffect(() => {
-    const fetchProductCategories = async () => {
+    const getCategories = async () => {
       try {
-        const productCategoriesResponse = await getProductCategories()
-        setProductCategories(productCategoriesResponse.data)
+        const response = await getProductCategories()
+        if (response && response.status === 200) {
+          setProductCategories(response.data)
+        }
       } catch (error) {
+        setNotification("Can not get product categories")
         console.error("Can not get product categories ", error)
       }
     }
-    fetchProductCategories()
+    getCategories()
   }, [])
 
-  return { productCategories }
+  return { productCategories, notification }
 }
