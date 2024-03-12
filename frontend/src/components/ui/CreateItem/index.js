d34206e1156fa946f53b4f146f034da5d308a351
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-03-12 16:36:34 +0100

    extract toggle button from form adapt for sign up page

--- frontend/src/components/ui/CreateItem/index.js
+++ frontend/src/components/ui/CreateItem/index.js
@@ -1,14 +1,12 @@
 import { ActionButton } from "#components/ui/ActionButton"
 import { FormInput } from "#components/ui/FormInput"
 import NotificationCard from "#components/ui/NotificationCard"
-import { ToggleButton } from "#components/ui/ToggleButton"
 import { STYLES } from "#constants/styles"
 import { useTranslation } from "#hooks/useTranslation"
 import { listenInputChangeAndSetDataObject } from "#utils/listenInputChangeAndSetDataObject"
 import { setFileToUpload } from "#utils/setFileToUpload"
 import { useState } from "react"
 import { OptionsFormInput } from "./OptionsFormInput"
-
 import styles from "./createItem.module.scss"
 
 export const CreateItem = ({
@@ -23,7 +21,6 @@ export const CreateItem = ({
     return acc
   }, {})
 
-  const [isVisible, setIsVisible] = useState(false)
   const [notification, setNotification] = useState(null)
   const [file, setFile] = useState(null)
 
@@ -35,53 +32,45 @@ export const CreateItem = ({
   return (
     <section aria-label='create item form'>
       {notification && <NotificationCard message={notification} />}
-      <ToggleButton
-        isVisible={isVisible}
-        onToggle={setIsVisible}
-        hideBtnText={components.createItem.hideButton.toUpperCase()}
-        showBtnText={components.createItem.showButton.toUpperCase()}
-        classCss={STYLES.BUTTONS.SHOW_HIDE}
-      />
-      {isVisible && (
-        <form className={styles.form}>
-          {Object.keys(itemData).map((field) =>
-            field !== "productCategoryId" ? (
-              <FormInput
-                classCss={STYLES.FORMS.FIELD}
-                key={field}
-                name={field}
-                onChangeListener={
-                  field === "image"
-                    ? (e) => setFileToUpload(e, setFile)
-                    : (e) => onChange(e, itemData, setItemData, setNotification)
-                }
-                placeholder={field}
-                label={field}
-                type={field === "image" && "file"}
-                value={itemData[field]}
-              />
-            ) : (
-              <OptionsFormInput
-                itemData={itemData}
-                setItemData={setItemData}
-                onChange={listenInputChangeAndSetDataObject}
-                setNotification={setNotification}
-                itemCategories={itemCategories}
-                property={"productCategoryId"}
-              />
-            ),
-          )}
-          <ActionButton
-            action={async (e) => {
-              await onCreate(e, itemData, setNotification, file, setCounter)
-              setItemData({ ...initialItemData })
-            }}
-            text={components.createItem.submitButton}
-            className={STYLES.BUTTONS.ACTION}
-            ariaLabel={"create item"}
-          />
-        </form>
-      )}
+
+      <form className={styles.form}>
+        {Object.keys(itemData).map((field) =>
+          field !== "productCategoryId" ? (
+            <FormInput
+              classCss={STYLES.FORMS.FIELD}
+              key={field}
+              name={field}
+              onChangeListener={
+                field === "image"
+                  ? (e) => setFileToUpload(e, setFile)
+                  : (e) => onChange(e, itemData, setItemData, setNotification)
+              }
+              placeholder={field}
+              label={field}
+              type={field === "image" ? "image" : "text"}
+              value={itemData[field]}
+            />
+          ) : (
+            <OptionsFormInput
+              itemData={itemData}
+              setItemData={setItemData}
+              onChange={listenInputChangeAndSetDataObject}
+              setNotification={setNotification}
+              itemCategories={itemCategories}
+              property={"productCategoryId"}
+            />
+          ),
+        )}
+        <ActionButton
+          action={async (e) => {
+            await onCreate(e, itemData, setNotification, file, setCounter)
+            setItemData({ ...initialItemData })
+          }}
+          text={components.createItem.submitButton}
+          className={STYLES.BUTTONS.ACTION}
+          ariaLabel={"create item"}
+        />
+      </form>
     </section>
   )
 }
