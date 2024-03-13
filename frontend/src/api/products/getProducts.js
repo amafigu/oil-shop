bc947f84197546bea380b433cfd92fc31db6002d
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-03-13 10:36:52 +0100

    use getProducts in context

--- frontend/src/api/products/getProducts.js
+++ frontend/src/api/products/getProducts.js
@@ -1,16 +1,13 @@
 import { API_PRODUCTS } from "#constants/api"
 import axios from "axios"
 
-export const getAllProducts = async () => {
+export const getProducts = async () => {
   try {
     const response = await axios.get(
       `${process.env.REACT_APP_API_URL}${API_PRODUCTS}`,
       { withCredentials: false },
     )
-
-    if (response && response.status === 200) {
-      return response.data
-    }
+    return response
   } catch (error) {
     throw error
   }
