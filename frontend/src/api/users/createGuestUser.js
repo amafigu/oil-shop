b3d4e591f7b3c29e65b33fbe69f4b5d593588ce1
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-03-15 00:38:48 +0100

    create requests for checkout process

---
+++ frontend/src/api/users/createGuestUser.js
@@ -0,0 +1,14 @@
+import { API_USERS_CREATE_GUEST } from "#constants/api"
+import axios from "axios"
+
+export const createGuestUser = async (guestUser) => {
+  try {
+    const response = await axios.post(
+      `${process.env.REACT_APP_API_URL}${API_USERS_CREATE_GUEST}`,
+      guestUser,
+    )
+    return response
+  } catch (error) {
+    throw error
+  }
+}
--- frontend/src/api/users/getUserShippingData.js
+++ frontend/src/api/users/getUserShippingData.js
@@ -2,9 +2,13 @@ import { API_SHIPPING_DATA } from "#constants/api"
 import axios from "axios"
 
 export const getUserShippingData = async (userId) => {
-  const response = await axios.get(
-    `${process.env.REACT_APP_API_URL}${API_SHIPPING_DATA}/${userId}`,
-    { withCredentials: true },
-  )
-  return response.data
+  try {
+    const response = await axios.get(
+      `${process.env.REACT_APP_API_URL}${API_SHIPPING_DATA}/${userId}`,
+      { withCredentials: true },
+    )
+    return response
+  } catch (error) {
+    throw error
+  }
 }
