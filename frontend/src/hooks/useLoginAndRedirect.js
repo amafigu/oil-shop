c2177f980e596e6a97b31479f8a42bb701cbe54e
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-03-15 16:24:57 +0100

    refactor login hook

--- frontend/src/hooks/useLoginAndRedirect.js
+++ frontend/src/hooks/useLoginAndRedirect.js
@@ -1,26 +1,24 @@
-import { loginUser } from "#api/auth/loginUser"
 import { API_USERS_CURRENT_PREFIX } from "#constants/api"
-import { LONG_MESSAGE_TIMEOUT, REDIRECT_TIMEOUT } from "#constants/time"
+import { REDIRECT_TIMEOUT } from "#constants/time"
 import useUserContext from "#context/userContext"
-import { useTranslation } from "#hooks/useTranslation"
+import { onLogin } from "#utils/onLogin"
+import { onRequestHandlerError } from "#utils/onRequestHandlerError"
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 
 export const useLoginAndRedirect = () => {
   const { setIsLoggedIn, setUserEmail, setUser } = useUserContext()
-  const [errorMessage, setErrorMessage] = useState("")
+  const [notification, setNotification] = useState(null)
   const [showPassword, setShowPassword] = useState(false)
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
-  const { translate } = useTranslation()
-  const text = translate.pages.login
   const navigate = useNavigate()
 
   const loginUserAndSetState = async (e) => {
     e.preventDefault()
 
     try {
-      const loginUserResponse = await loginUser(email, password)
+      const loginUserResponse = await onLogin(email, password)
       if (loginUserResponse) {
         setUserEmail(loginUserResponse.userEmail)
         setIsLoggedIn(loginUserResponse.isLoggedIn)
@@ -34,14 +32,14 @@ export const useLoginAndRedirect = () => {
         )
       }
     } catch (error) {
-      setErrorMessage(`${text.errorMessage}`)
-      setTimeout(() => setErrorMessage(null), LONG_MESSAGE_TIMEOUT)
-      console.error("Login error", error)
+      const message =
+        "There is an error by loggin, please check your credentials"
+      onRequestHandlerError(error, setNotification, message)
     }
   }
 
   return {
-    errorMessage,
+    notification,
     loginUserAndSetState,
     setEmail,
     setPassword,
