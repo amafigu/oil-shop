ea6f539d011d8256363b59090399ae8bab6b6c0d
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-03-15 16:23:52 +0100

    login and user roles requests

---
+++ frontend/src/api/auth/login.js
@@ -0,0 +1,17 @@
+import { API_LOGIN } from "#constants/api"
+import axios from "axios"
+
+export const login = async (email, password) => {
+  try {
+    const response = await axios.post(
+      `${process.env.REACT_APP_API_URL}${API_LOGIN}`,
+      { email, password },
+      { withCredentials: true },
+    )
+    if (response && response.status === 200) {
+      return response
+    }
+  } catch (error) {
+    throw error
+  }
+}
