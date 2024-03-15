7f1b05c9af846a180c54e7aaefbf8bbd1b8777f3
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-03-15 18:02:12 +0100

    use try in request

--- frontend/src/api/orders/getUserOrderItemsRequest.js
+++ frontend/src/api/orders/getUserOrderItemsRequest.js
@@ -2,10 +2,13 @@ import { API_ORDERS_CART_ITEMS } from "#constants/api"
 import axios from "axios"
 
 export const getUserOrderItemsRequest = async (orderId) => {
-  const response = await axios.get(
-    `${process.env.REACT_APP_API_URL}${API_ORDERS_CART_ITEMS}/${orderId}`,
-
-    { withCredentials: true },
-  )
-  return response
+  try {
+    const response = await axios.get(
+      `${process.env.REACT_APP_API_URL}${API_ORDERS_CART_ITEMS}/${orderId}`,
+      { withCredentials: true },
+    )
+    return response
+  } catch (error) {
+    throw error
+  }
 }
