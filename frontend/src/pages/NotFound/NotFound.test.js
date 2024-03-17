8ce599ad5bc6a33da26964bf9f20561100c5d949
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-03-17 19:34:03 +0100

    fix old tests

--- frontend/src/pages/NotFound/NotFound.test.js
+++ frontend/src/pages/NotFound/NotFound.test.js
@@ -1,16 +1,14 @@
 import { translate } from "#__mocks__/translate"
 import { useTranslation } from "#hooks/useTranslation"
 import { NotFound } from "#pages/NotFound"
-import { scrollToTop } from "#utils/scrollToTop"
 import "@testing-library/jest-dom"
 import { render, screen } from "@testing-library/react"
 
 jest.mock("#hooks/useTranslation")
-jest.mock("#utils/render")
+jest.mock("#utils/scrollToTop")
 
 describe("Faq page should", () => {
   beforeAll(() => {
-    scrollToTop.mockReturnValue(jest.fn())
     useTranslation.mockReturnValue({ translate })
   })
 
