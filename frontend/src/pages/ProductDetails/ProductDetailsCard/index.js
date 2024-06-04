da3521fc86c610d1f6634ec4c679359a8180c03a
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-06-04 17:43:28 +0200

    refactor productDetailsCard

--- frontend/src/pages/ProductDetails/ProductDetailsCard/index.js
+++ frontend/src/pages/ProductDetails/ProductDetailsCard/index.js
@@ -1,9 +1,9 @@
 import { ActionButton } from "#components/ui/ActionButton"
-import NotificationCard from "#components/ui/NotificationCard"
 import { DEFAULT_PRODUCT_IMAGE } from "#constants/media"
-import { SHOP_QUERY_CATEGORY_PREFIX } from "#constants/routes"
+import { SHOP } from "#constants/routes"
 import { STYLES } from "#constants/styles"
 import { useCart } from "#hooks/useCart"
+import { useProductCategory } from "#hooks/useProductCategory"
 import { useProductDetails } from "#hooks/useProductDetails"
 import { useTranslation } from "#hooks/useTranslation"
 import { setDefaultImageByError } from "#utils/setDefaultImageByError"
@@ -13,13 +13,13 @@ import { Counter } from "./Counter"
 import styles from "./productDetailsCard.module.scss"
 
 export const ProductDetailsCard = () => {
-  const { product, quantity, setQuantity, notification } = useProductDetails()
+  const { product, quantity, setQuantity } = useProductDetails()
   const { addProduct } = useCart()
   const { components } = useTranslation()
+  const { setSortCategory } = useProductCategory()
 
   return (
     <>
-      {notification && <NotificationCard message={notification} />}
       {product && (
         <article
           className={styles.wrapper}
@@ -37,25 +37,26 @@ export const ProductDetailsCard = () => {
               />
             </div>
             <div className={styles.container}>
-              <ul className={styles.data}>
+              <ul className={styles.list}>
                 <li
                   className={styles.category}
                   aria-label={`link to ${product.category.name} products`}
                 >
                   <Link
                     className={styles.link}
-                    to={`${SHOP_QUERY_CATEGORY_PREFIX}${product.category.name}`}
+                    to={SHOP}
+                    onClick={() => setSortCategory(product.category.name)}
                   >
-                    {titleCase(product.category.name, "_")}
+                    {titleCase(product.category.name, " ")}
                   </Link>
                 </li>
 
                 <li className={styles.name}>
-                  {titleCase(product.name, "_")} {product.size} ml
+                  {titleCase(product.name, " ")} {product.size} ml
                 </li>
                 <li className={styles.description}>{product.description}</li>
                 <li className={styles.details}>{product.details}</li>
-                <li className={styles.row}>
+                <li className={styles.detailsRow}>
                   <span className={styles.size}>{product.size} ml</span>
                   <span className={styles.price}>${product.price}</span>
                 </li>
