673aa7d451fd140ed85b77267279a6c44a702178
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-06-04 17:35:00 +0200

    delete unused constants

--- frontend/src/constants/products.js
+++ frontend/src/constants/products.js
@@ -1,35 +1,7 @@
-export const initialProductData = {
-  name: "",
-  description: "",
-  price: "",
-  image: "",
-  size: "",
-}
-
-export const emptyCreateProductObj = {
-  name: "",
-  category: "",
-  description: "",
-  price: "",
-  details: "",
-  size: "",
-  image: "",
-}
-
 export const editableProductProperties = [
   "name",
   "description",
   "price",
-  "image",
-  "size",
-]
-
-export const createProductProperties = [
-  "name",
-  "category",
-  "description",
-  "price",
-  "details",
   "image",
   "size",
 ]
