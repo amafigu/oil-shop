601d2668b07b8626a02d651bf6cf947ecbea7060
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-06-05 23:48:39 +0200

    delete unused function

--- frontend/src/hooks/useCurrentUser.js
+++
@@ -1,7 +0,0 @@
-import useUserContext from "#context/userContext"
-
-export const useCurrentUser = () => {
-  const { setIsLoggedIn, setUser, user, isLoggedIn } = useUserContext()
-
-  return { setIsLoggedIn, setUser, user, isLoggedIn }
-}
