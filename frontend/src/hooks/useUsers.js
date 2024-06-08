312b6b2835f1af7532d74f32b68f31f8d8a3bb65
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-06-08 13:35:43 +0200

    change names and delete functions

--- frontend/src/hooks/useUsers.js
+++
@@ -1,7 +0,0 @@
-import { useUserContext } from "#context/userContext"
-
-export const useUsers = () => {
-  const { users, setUsers, updateUser, deleteUser, addUser } = useUserContext()
-
-  return { users, setUsers, updateUser, deleteUser, addUser }
-}
