ff59aa4bac489170daefb7785eb1d3615d780dd1
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-06-10 12:40:27 +0200

    helper validation functions

--- frontend/src/utils/verifyTypes.js
+++ frontend/src/utils/verifyTypes.js
@@ -1,10 +1,46 @@
-export const convertToExpectedTypes = async (data) => {
+export const convertDataToExpectedUserTypes = (data) => {
   return {
     ...data,
     firstName: String(data.firstName),
     lastName: String(data.lastName),
     email: String(data.email),
     password: String(data.password),
+  }
+}
+
+export const convertDataToExpectedProductTypes = async ({
+  data,
+  file,
+  verifyImgUrl,
+  upload,
+}) => {
+  return {
+    ...data,
+    image: await verifyImgUrl(file, upload),
+    details: String(data.details),
+    description: String(data.description),
+    categoryId: Number(data.categoryId),
+    size: Number(data.size),
+    price: Number(data.price),
+  }
+}
+
+export const extractValidProperty = async ({
+  key,
+  updatedData,
+  upload,
+  file,
+}) => {
+  if (key === "image" && file) {
+    const image = await upload(file)
+    return { [key]: image }
+  } else {
+    const value = updatedData[key]
+    if (key === "price" || key === "size") {
+      return { [key]: Number(value) }
+    } else {
+      return { [key]: value }
+    }
   }
 }
 
