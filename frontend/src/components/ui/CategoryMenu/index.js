205b4d79e7a933c1a3c96ec00b56f514bf3f53bb
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-06-11 00:33:13 +0200

    add conditional to onSelect

--- frontend/src/components/ui/CategoryMenu/index.js
+++ frontend/src/components/ui/CategoryMenu/index.js
@@ -12,20 +12,34 @@ export const CategoryMenu = () => {
 
   const onSelect = (e, item) => {
     e.preventDefault()
-    setSortCategory(item.name)
+    if (item) {
+      setSortCategory(item.name)
+    } else {
+      setSortCategory(null)
+    }
     setShowMobileMenu(false)
   }
 
   return (
     <nav className={styles.categories} aria-label='product categories selector'>
       <ul className={styles.list}>
+        <li className={styles.item}>
+          <ActionButton
+            action={(e) => onSelect(e)}
+            text={components.categoryMenu.all}
+            className={
+              sortCategory === null ? "navigationActive" : "navigation"
+            }
+            aria-label='category filter item'
+          />
+        </li>
         {categories &&
           categories.map((item, index) => {
             return (
               <li className={styles.item} key={index}>
                 <ActionButton
                   action={(e) => onSelect(e, item)}
-                  text={components.navigationMenu[camelCase(item.name, " ")]}
+                  text={components.categoryMenu[camelCase(item.name, " ")]}
                   className={
                     item.name === sortCategory
                       ? "navigationActive"
