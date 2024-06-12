6a5d3d95f9ea8492ef61d1cadd47b47b0492d8b5
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-06-12 19:31:00 +0200

    feat: user header reflects changes in current user

--- frontend/src/components/ui/UserHeader/index.js
+++ frontend/src/components/ui/UserHeader/index.js
@@ -2,26 +2,37 @@ import { DEFAULT_USER_IMAGE } from "#constants/media"
 import { useUserContext } from "#context/userContext"
 import { useTranslation } from "#hooks/useTranslation"
 import { setDefaultImageByError } from "#utils/setDefaultImageByError"
+import { useEffect, useState } from "react"
 import styles from "./userHeader.module.scss"
 
 export const UserHeader = () => {
-  const { user } = useUserContext()
+  const [userData, setUserData] = useState()
+  const { user, isLoading } = useUserContext()
   const { commonProperties } = useTranslation()
-  const userData = {
-    firstName: user?.firstName,
-    lastName: user?.lastName,
-    email: user?.email,
-  }
+
+  useEffect(() => {
+    if (isLoading) {
+      return
+    } else {
+      setUserData({
+        firstName: user?.firstName,
+        lastName: user?.lastName,
+        email: user?.email,
+        image: user?.image,
+      })
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user])
 
   return (
     <header className={styles.wrapper} aria-label='User Description'>
       <section className={styles.container}>
-        {user ? (
+        {userData ? (
           <div className={styles.content}>
             <div className={styles.imageContainer}>
               <img
                 className={styles.image}
-                src={user.image}
+                src={userData.image}
                 alt='user'
                 onError={(e) => setDefaultImageByError(e, DEFAULT_USER_IMAGE)}
               />
@@ -29,14 +40,16 @@ export const UserHeader = () => {
             <dl className={styles.fields}>
               <ul className={styles.list}>
                 {userData &&
-                  Object.keys(userData).map((property) => (
-                    <li key={property} className={styles.item}>
-                      <dt
-                        className={styles.property}
-                      >{`${commonProperties[property]}: `}</dt>
-                      <dd className={styles.value}>{userData[property]}</dd>
-                    </li>
-                  ))}
+                  Object.keys(userData)
+                    .filter((property) => property !== "image")
+                    .map((property) => (
+                      <li key={property} className={styles.item}>
+                        <dt
+                          className={styles.property}
+                        >{`${commonProperties[property]}: `}</dt>
+                        <dd className={styles.value}>{userData[property]}</dd>
+                      </li>
+                    ))}
               </ul>
             </dl>
           </div>
