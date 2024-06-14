f0c30aa617e7cf3b13df1dbcc7a36317e587c675
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-06-14 19:24:30 +0200

    refactor: add types and adjust MatchedProductsList

--- frontend/src/components/ui/Navbar/MatchedProductsList/index.tsx
+++ frontend/src/components/ui/Navbar/MatchedProductsList/index.tsx
@@ -1,12 +1,42 @@
-import { DEFAULT_PRODUCT_IMAGE } from "#constants/media"
-import { useMenuOptions } from "#hooks/useMenuOptions"
-import { navigateAndClose } from "#utils/navigateAndClose"
-import { setDefaultImageByError } from "#utils/setDefaultImageByError"
-import { titleCase } from "#utils/titleCase"
+import { DEFAULT_PRODUCT_IMAGE } from "@/constants/media"
+import { PRODUCTS } from "@/constants/routes"
+import { useMenuOptions } from "@/hooks/useMenuOptions"
+import { Product } from "@/types/Product"
+import { setDefaultImageByError } from "@/utils/setDefaultImageByError"
+import { titleCase } from "@/utils/titleCase"
+import { Dispatch, FC, SetStateAction } from "react"
 import { useNavigate } from "react-router-dom"
 import styles from "./matchedProductsList.module.scss"
 
-export const MatchedProductsList = ({
+interface Navigate {
+  productId: number
+  navigate: (route: string) => void
+  setItemsList: Dispatch<SetStateAction<Product[]>>
+  setShowList: Dispatch<SetStateAction<boolean>>
+  setSearchProductText: Dispatch<SetStateAction<string>>
+}
+
+interface MatchedProductsListProps {
+  matchedProducts: Product[]
+  setMatchedProducts: Dispatch<SetStateAction<Product[]>>
+  setShowMatchedProductsList: Dispatch<SetStateAction<boolean>>
+  setSearchProductText: Dispatch<SetStateAction<string>>
+}
+
+const navigateAndClose = ({
+  productId,
+  navigate,
+  setItemsList,
+  setShowList,
+  setSearchProductText,
+}: Navigate) => {
+  navigate(`${PRODUCTS}/${productId}`)
+  setItemsList([])
+  setShowList(false)
+  setSearchProductText("")
+}
+
+export const MatchedProductsList: FC<MatchedProductsListProps> = ({
   matchedProducts,
   setMatchedProducts,
   setShowMatchedProductsList,
@@ -22,26 +52,26 @@ export const MatchedProductsList = ({
           className={styles.item}
           aria-label={`Searched item: ${product.name}`}
           key={product.id}
-          onClick={() =>
-            navigateAndClose(
-              product.id,
+          onClick={() => {
+            navigateAndClose({
+              productId: product.id,
               navigate,
-              setMatchedProducts,
-              setShowMatchedProductsList,
+              setItemsList: setMatchedProducts,
+              setShowList: setShowMatchedProductsList,
               setSearchProductText,
-              setShowProductsSearchBar(false),
-            )
-          }
+            })
+            setShowProductsSearchBar(false)
+          }}
           onKeyDown={(e) => {
             if (e.key === "Enter") {
-              navigateAndClose(
-                product.id,
+              navigateAndClose({
+                productId: product.id,
                 navigate,
-                setMatchedProducts,
-                setShowMatchedProductsList,
+                setItemsList: setMatchedProducts,
+                setShowList: setShowMatchedProductsList,
                 setSearchProductText,
-                setShowProductsSearchBar(false),
-              )
+              })
+              setShowProductsSearchBar(false)
             }
           }}
           tabIndex={0}
