644ff1a89a9aa1264a5a42252e20211f371fbfcd
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-06-17 01:25:01 +0200

    refactoring: add types to test

--- frontend/src/components/ui/CategoryMenu/CategoryMenu.test.tsx
+++ frontend/src/components/ui/CategoryMenu/CategoryMenu.test.tsx
@@ -1,38 +1,64 @@
-import { productCategories, sortCategory } from "#__mocks__/productCategories"
-import { components } from "#__mocks__/translate"
-import { useMenuOptions } from "#hooks/useMenuOptions"
-import { useProductCategory } from "#hooks/useProductCategory"
-import { useTranslation } from "#hooks/useTranslation"
-import { render, screen } from "@testing-library/react"
-import { CategoryMenu } from "./index"
+import { productCategories, sortCategory } from "@/__mocks__/productCategories"
+import { components } from "@/__mocks__/translate"
+import { CategoryMenu } from "@/components/ui/CategoryMenu"
+import { useMenuOptions } from "@/hooks/useMenuOptions"
+import { useProductCategory } from "@/hooks/useProductCategory"
+import { useTranslation } from "@/hooks/useTranslation"
+import { camelCase } from "@/utils/camelCase"
+import { fireEvent, render, screen } from "@testing-library/react"
 
-jest.mock("#hooks/useTranslation")
-jest.mock("#hooks/useProductCategory")
-jest.mock("#hooks/useMenuOptions")
+jest.mock("@/hooks/useTranslation")
+jest.mock("@/hooks/useProductCategory")
+jest.mock("@/hooks/useMenuOptions")
 
-describe("<CategoryMenu /> does", () => {
+describe("<CategoryMenu />", () => {
+  const setSortCategory = jest.fn()
+  const setShowMobileMenu = jest.fn()
+  const setShowProductsSearchBar = jest.fn()
+
   beforeEach(() => {
-    const setSortCategory = jest.fn()
-    const showMobileMenu = true
-    const setShowMobileMenu = jest.fn()
-    const showProductsSearchBar = true
-    const setShowProductsSearchBar = jest.fn()
-    useTranslation.mockReturnValue({ components })
-    useMenuOptions.mockReturnValue({
-      showMobileMenu,
+    ;(useTranslation as jest.Mock).mockReturnValue({ components })
+    ;(useMenuOptions as jest.Mock).mockReturnValue({
+      showMobileMenu: true,
       setShowMobileMenu,
-      showProductsSearchBar,
+      showProductsSearchBar: true,
       setShowProductsSearchBar,
     })
-    useProductCategory.mockReturnValue({
+    ;(useProductCategory as jest.Mock).mockReturnValue({
       setSortCategory,
-      productCategories,
+      categories: productCategories,
       sortCategory,
     })
   })
 
-  test("render All Products option", () => {
+  test("renders All Products option", () => {
     render(<CategoryMenu />)
-    expect(screen.getByText("All Products")).toBeInTheDocument()
+    expect(screen.getByText(components.categoryMenu.all)).toBeInTheDocument()
+  })
+
+  test("renders categories correctly", () => {
+    render(<CategoryMenu />)
+    productCategories.forEach((category) => {
+      const categoryKey = camelCase(category.name, " ")
+      expect(
+        screen.getByText(
+          components.categoryMenu[
+            categoryKey as keyof typeof components.categoryMenu
+          ],
+        ),
+      ).toBeInTheDocument()
+    })
+  })
+
+  test("calls setSortCategory when a category is clicked", () => {
+    render(<CategoryMenu />)
+    const categoryKey = camelCase(productCategories[0]?.name ?? "", " ")
+    const categoryButton = screen.getByText(
+      components.categoryMenu[
+        categoryKey as keyof typeof components.categoryMenu
+      ],
+    )
+    fireEvent.click(categoryButton)
+    expect(setSortCategory).toHaveBeenCalledWith(productCategories[0]?.name)
   })
 })
