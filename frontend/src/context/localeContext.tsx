ba6a0a4bbbd39e46280fe5b41ea0943dac60d0d8
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-06-16 10:46:20 +0200

    refactoring: add types to locale context

--- frontend/src/context/localeContext.tsx
+++ frontend/src/context/localeContext.tsx
@@ -1,21 +1,56 @@
-import React, { createContext, useContext, useState } from "react"
+import {
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  createContext,
+  useContext,
+  useState,
+} from "react"
 import de from "../i18n/de.json"
 import en from "../i18n/en.json"
 
 const languages = { en, de }
 
-export const LocaleContext = createContext()
+type Language = keyof typeof languages
 
-export const LocaleContextProvider = ({ children }) => {
-  const [language, setLanguage] = useState("en")
+interface LocaleContextTypes {
+  languages: typeof languages
+  setLanguage: Dispatch<SetStateAction<Language>>
+  language: Language
+  translate: (typeof languages)[Language]
+}
+
+export const LocaleContext = createContext<LocaleContextTypes | undefined>(
+  undefined,
+)
+
+export const LocaleContextProvider = ({
+  children,
+}: {
+  children: ReactNode
+}) => {
+  const [language, setLanguage] = useState<Language>("en")
 
   return (
     <LocaleContext.Provider
-      value={{ translate: languages[language], language, setLanguage }}
+      value={{
+        languages,
+        translate: languages[language],
+        language,
+        setLanguage,
+      }}
     >
       {children}
     </LocaleContext.Provider>
   )
 }
 
-export const useLocaleContext = () => useContext(LocaleContext)
+export const useLocaleContext = () => {
+  const context = useContext(LocaleContext)
+  if (context === undefined) {
+    throw new Error(
+      "useLocaleContext must be used within a LocaleContextProvider",
+    )
+  }
+  return context
+}
