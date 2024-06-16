79913607d3b6db7dd58c0ff5362bd2c310b7dae6
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-06-17 01:24:24 +0200

    refactoring: add types to editable items list

--- frontend/src/components/ui/EditableItemsList/index.tsx
+++ frontend/src/components/ui/EditableItemsList/index.tsx
@@ -1,17 +1,36 @@
-import { ActionButton } from "#components/ui/ActionButton"
-import { useTranslation } from "#hooks/useTranslation"
-import { useEffect, useState } from "react"
+import { ActionButton } from "@/components/ui/ActionButton"
+import { useTranslation } from "@/hooks/useTranslation"
+import { Order } from "@/types/Order"
+import { Product } from "@/types/Product"
+import { User } from "@/types/User"
+import { ComponentType, useEffect, useState } from "react"
 import styles from "./editableItemsList.module.scss"
 
-export const EditableItemsList = ({
+interface EditableItemsListProps<T> {
+  itemsList: T[]
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  ItemComponent: ComponentType<{ item: T; [key: string]: any }>
+  title: string
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  itemProps?: { [key: string]: any }
+  filterProps?: Array<{
+    action: (list: T[], sortIsAsc: boolean) => T[]
+    targetProperty: string
+    className: string
+  }>
+}
+
+export const EditableItemsList = <T extends User | Product | Order>({
   itemsList,
   ItemComponent,
   title,
   itemProps = {},
   filterProps = [],
-}) => {
-  const [filteredItemsList, setFilteredItemsList] = useState([])
-  const [isAscendent, setIsAscendent] = useState(filterProps.map(() => true))
+}: EditableItemsListProps<T>) => {
+  const [filteredItemsList, setFilteredItemsList] = useState<T[]>([])
+  const [isAscendent, setIsAscendent] = useState<boolean[]>(
+    filterProps.map(() => true),
+  )
   const { pages } = useTranslation()
   const text = pages.admin.usersManagement.editableItemsList
 
@@ -19,12 +38,18 @@ export const EditableItemsList = ({
     setFilteredItemsList(itemsList)
   }, [itemsList])
 
-  const filterList = (action, index) => {
+  const filterList = (
+    action: (list: T[], sortIsAsc: boolean) => T[],
+    index: number,
+  ) => {
     const sortIsAsc = [...isAscendent]
     sortIsAsc[index] = !sortIsAsc[index]
     setIsAscendent(sortIsAsc)
 
-    const filteredItems = action([...filteredItemsList], sortIsAsc[index])
+    const filteredItems = action(
+      [...filteredItemsList],
+      sortIsAsc[index] ?? true,
+    )
     setFilteredItemsList(filteredItems)
   }
 
@@ -32,36 +57,31 @@ export const EditableItemsList = ({
     <section className={styles.wrapper} aria-label={title}>
       <h2>{title}</h2>
       <div className={styles.container}>
-        {filteredItemsList &&
-          filterProps &&
-          filterProps.length > 0 &&
-          filterProps.map((filterButton, index) => {
-            return (
-              <ActionButton
-                action={() => filterList(filterButton.action, index)}
-                text={
-                  isAscendent[index] === true
-                    ? `${text[filterButton.targetProperty]} asc`
-                    : `${text[filterButton.targetProperty]} des`
-                }
-                className={filterButton.className}
-                ariaLabel='Filter button'
-                key={index}
-              />
-            )
-          })}
+        {filterProps.length > 0 &&
+          filterProps.map((filterButton, index) => (
+            <ActionButton
+              key={index}
+              action={() => filterList(filterButton.action, index)}
+              text={
+                isAscendent[index]
+                  ? `${text[filterButton.targetProperty]} asc`
+                  : `${text[filterButton.targetProperty]} des`
+              }
+              className={filterButton.className}
+              ariaLabel='Filter button'
+            />
+          ))}
       </div>
-
-      {
-        <ul className={styles.list}>
-          {filteredItemsList &&
-            filteredItemsList.map((item, index) => (
-              <li className={styles.item} key={item.id || index}>
-                <ItemComponent item={item} {...itemProps} />
-              </li>
-            ))}
-        </ul>
-      }
+      <ul className={styles.list}>
+        {filteredItemsList.map((item, index) => (
+          <li
+            className={styles.item}
+            key={(item as User).id || (item as Product).id || index}
+          >
+            <ItemComponent item={item} {...itemProps} />
+          </li>
+        ))}
+      </ul>
     </section>
   )
 }
