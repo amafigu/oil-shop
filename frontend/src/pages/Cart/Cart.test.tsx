077ed07087dee4cb2f28776959d52c3096ed6b52
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-06-17 01:22:51 +0200

    refactoring: add types Cart test

--- frontend/src/pages/Cart/Cart.test.tsx
+++ frontend/src/pages/Cart/Cart.test.tsx
@@ -1,66 +1,47 @@
-import cartItem from "#__mocks__/cartItem"
-import { translate } from "#__mocks__/translate"
-import { useUserContext } from "#context/userContext"
-import { useCart } from "#hooks/useCart"
-import { useTranslation } from "#hooks/useTranslation"
-import { Cart } from "#pages/Cart"
+import { translate } from "@/__mocks__/translate"
+import { CartProvider } from "@/context/cartContext"
+import { useUserContext } from "@/context/userContext"
+import { useTranslation } from "@/hooks/useTranslation"
+import { Cart } from "@/pages/Cart"
 import "@testing-library/jest-dom"
 import { fireEvent, render, screen } from "@testing-library/react"
-import React from "react"
 import { MemoryRouter } from "react-router-dom"
 
-jest.mock("#hooks/useCart")
-jest.mock("#hooks/useTranslation")
-jest.mock("#context/userContext")
+jest.mock("@/hooks/useTranslation")
+jest.mock("@/context/userContext")
 
 function renderCart() {
   render(
     <MemoryRouter>
-      <Cart />
+      <CartProvider>
+        <Cart />
+      </CartProvider>
     </MemoryRouter>,
   )
 }
+
 describe("Cart should", () => {
-  let mockRemoveProduct
+  let mockRemoveProduct: jest.Mock
+
   beforeEach(() => {
     window.scrollTo = jest.fn()
-    mockRemoveProduct = jest.fn()
-
-    useUserContext.mockReturnValue({})
-    useCart.mockReturnValue({
-      cart: [cartItem],
-      removeProduct: mockRemoveProduct,
-      updateProductQuantity: jest.fn(),
-    })
-    useTranslation.mockReturnValue({
-      translate,
-    })
+    mockRemoveProduct = jest
+      .fn()(useUserContext as jest.Mock)
+      .mockReturnValue({})(useTranslation as jest.Mock)
+      .mockReturnValue({ translate })
   })
 
   test("call remove product", () => {
     renderCart()
-    fireEvent.click(screen.getByLabelText("Delete Test Product from cart"))
+    const button = screen.getByLabelText("Delete Test Product from cart")
+    fireEvent.click(button)
     expect(mockRemoveProduct).toHaveBeenCalledWith("Test Product")
   })
+
   test("render products correctly", () => {
     renderCart()
-    useCart.mockReturnValue({
-      cart: [
-        {
-          product: {
-            name: "Test Product",
-            image: "image.jpg",
-            description: "Description",
-            size: 100,
-            price: 10,
-          },
-          quantity: 2,
-        },
-      ],
-      removeProduct: jest.fn(),
-      updateProductQuantity: jest.fn(),
-    })
-
     expect(screen.getByText("Test Product")).toBeInTheDocument()
+    expect(screen.getByText("10 €")).toBeInTheDocument()
+    expect(screen.getByText("100 ml")).toBeInTheDocument()
   })
 })
