69e1ac21296076c152ceea7604b1b0237060747e
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-06-16 12:32:41 +0200

    refactor: add types to SortedProductsList

--- frontend/src/pages/Shop/SortedProductsList/index.tsx
+++ frontend/src/pages/Shop/SortedProductsList/index.tsx
@@ -1,33 +1,46 @@
-import { ProductCard } from "#components/products/ProductCard"
-import { useProductContext } from "#context/productContext"
-import { useProductCategory } from "#hooks/useProductCategory"
-import { useEffect, useState } from "react"
+import { ProductCard } from "@/components/products/ProductCard"
+import { useProductContext } from "@/context/productContext"
+import { useProductCategory } from "@/hooks/useProductCategory"
+import { Category, Product } from "@/types/Product"
+import { FC, useEffect, useState } from "react"
 import styles from "./sortedProductsList.module.scss"
 
-export const SortedProductsList = () => {
-  const [productsList, setProductsList] = useState([])
+export const SortedProductsList: FC = () => {
+  const [productsList, setProductsList] = useState<Product[]>([])
   const { products } = useProductContext()
   const { sortCategory, categories } = useProductCategory()
 
   useEffect(() => {
-    const sortByCategory = (products, category) => {
-      const categoryNames = categories.map((category) => category.name)
+    const sortByCategory = (
+      products: Product[],
+      category: string,
+    ): Product[] => {
+      const categoryNames = categories.map(
+        (category: Category) => category.name,
+      )
       if (categoryNames.includes(category)) {
         return products.filter(
-          (product) => category && product.category.name === category,
+          (product) =>
+            category && product.category && product.category.name === category,
         )
       } else {
         return products
       }
     }
-    setProductsList(sortByCategory(products, sortCategory))
+
+    if (sortCategory) {
+      setProductsList(sortByCategory(products, sortCategory))
+    } else {
+      setProductsList(products)
+    }
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [products, sortCategory])
 
   return (
     <ul className={styles.container} aria-label='products list'>
       {productsList &&
-        productsList.map((product) => (
+        productsList.map((product: Product) => (
           <li key={product.id} aria-label='products list item'>
             <ProductCard product={product} />
           </li>
