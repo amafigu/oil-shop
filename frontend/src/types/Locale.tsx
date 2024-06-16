ca602f59f474463e7f003ee469a8ebe70a5ac17c
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-06-17 01:17:37 +0200

    refactoring: add locale types

--- frontend/src/types/Locale.tsx
+++ frontend/src/types/Locale.tsx
@@ -2,10 +2,8 @@ import { Language, languages } from "@/context/localeContext"
 import { Dispatch, SetStateAction } from "react"
 
 export interface Translation {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  components: any
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  pages: any
+  components: Components
+  pages: Pages
   commonButtons: CommonButtons
   commonProperties: CommonProperties
   language: Language
@@ -13,10 +11,17 @@ export interface Translation {
   translate: (typeof languages)[Language]
 }
 
-interface CommonButtons {
+export interface CommonButtons {
   [key: string]: string
 }
-
-interface CommonProperties {
+export interface Pages {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [key: string]: any
+}
+export interface Components {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [key: string]: any
+}
+export interface CommonProperties {
   [key: string]: string
 }
