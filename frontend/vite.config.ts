ae68c9218f14685e5371ccc0cc28c90a7898cdde
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-07-01 12:46:33 +0200

    config: add vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest" />
+
 import react from "@vitejs/plugin-react"
 import { fileURLToPath } from "url"
 import { defineConfig } from "vite"
@@ -6,7 +8,11 @@ import viteTsconfigPaths from "vite-tsconfig-paths"
 export default defineConfig({
   base: "/",
   plugins: [react(), viteTsconfigPaths()],
-
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./setupTests.ts"],
+  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
