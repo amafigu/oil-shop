5322de72098667826920a7fc5526e41f1ea18461
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-07-02 15:01:18 +0200

    refactor: adjust types

--- frontend/src/utils/listenInput.tsx
+++ frontend/src/utils/listenInput.tsx
@@ -1,5 +1,5 @@
 import { ChangeEvent, Dispatch, SetStateAction } from "react"
-
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const listenInput = <T extends { [key: string]: any }>(
   e: ChangeEvent<HTMLInputElement | HTMLSelectElement>,
   updatedDataObj: T,
--- frontend/src/utils/translateByPath.tsx
+++ frontend/src/utils/translateByPath.tsx
@@ -1,12 +1,12 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 export const translateByPath = (
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   translationObj: { [key: string]: any },
   path: string,
-) => {
+): string => {
   const splitedPathArray = path.split(".")
   const reducedObj = splitedPathArray.reduce(
     (acc, property) => (acc ? acc[property] : undefined),
     translationObj,
   )
-  return reducedObj
+  return typeof reducedObj === "string" ? reducedObj : path
 }
