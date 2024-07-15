3199c3c4b230cee615a5731b78ea7cb50c0f663a
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-07-15 17:51:53 +0200

    add test notificaionCard

--- frontend/src/components/ui/NotificationCard/index.tsx
+++ frontend/src/components/ui/NotificationCard/index.tsx
@@ -1,18 +1,11 @@
 import { FC } from "react"
-import { ValidationErrorsCard } from "./ValidationErrorsCard"
 import styles from "./notificationCard.module.scss"
 
 interface NotificationCardProps {
   message: string
-  errorsMessageArray?: { message: string; path: string }[]
-  translatedValidationErrors?: { [key: string]: { [key: string]: string } }
 }
 
-export const NotificationCard: FC<NotificationCardProps> = ({
-  message,
-  errorsMessageArray = [],
-  translatedValidationErrors = {},
-}) => {
+export const NotificationCard: FC<NotificationCardProps> = ({ message }) => {
   return (
     <article className={styles.container}>
       {message && message !== "" && (
@@ -20,19 +13,6 @@ export const NotificationCard: FC<NotificationCardProps> = ({
           <div className={styles.card}>{message}</div>
         </div>
       )}
-
-      {translatedValidationErrors &&
-        errorsMessageArray &&
-        errorsMessageArray.length > 0 && (
-          <div className={styles.modal}>
-            <div className={styles.card}>
-              <ValidationErrorsCard
-                errorsArray={errorsMessageArray}
-                translationObj={translatedValidationErrors}
-              />
-            </div>
-          </div>
-        )}
     </article>
   )
 }
