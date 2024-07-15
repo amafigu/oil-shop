2af093ce8f103d27090ebc4d01555b78f6d5e006
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-07-15 17:54:14 +0200

    clean types

--- frontend/src/types/Notification.tsx
+++ frontend/src/types/Notification.tsx
@@ -2,3 +2,8 @@ export interface NotificationContextType {
   setNotification: (message: string | null) => void
   onSetNotification: (message: string) => void
 }
+
+export interface ValidationErrorsCardProps {
+  errorsArray: { message: string; path: string }[]
+  translationObj: { [key: string]: { [key: string]: string } }
+}
