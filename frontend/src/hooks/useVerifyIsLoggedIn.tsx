9a111088447fbc63d4feaa8fe8e15ceeb3a99a56
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-07-17 12:48:46 +0200

    add verify login hook

---
+++ frontend/src/hooks/useVerifyIsLoggedIn.tsx
@@ -0,0 +1,22 @@
+import { LOGIN } from "@/constants/routes"
+import { useUserContext } from "@/context/userContext"
+import { useNavigate } from "react-router-dom"
+
+export const useVerifyIsLoggedIn = () => {
+  const { isLoading, isLoggedIn } = useUserContext()
+  const navigate = useNavigate()
+
+  const verifyIsLoggedIn = () => {
+    if (isLoading) {
+      return
+    } else {
+      if (!isLoggedIn) {
+        navigate(LOGIN)
+      } else {
+        return
+      }
+    }
+  }
+
+  return { verifyIsLoggedIn }
+}
