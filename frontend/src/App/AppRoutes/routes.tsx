d308df972c7331275bdf8b1d215570f5f4b36356
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-07-17 17:44:08 +0200

    add routes

--- frontend/src/App/AppRoutes/routes.tsx
+++ frontend/src/App/AppRoutes/routes.tsx
@@ -12,13 +12,19 @@ import {
   ORDER_SUMMARY,
   PAYMENT,
   PRODUCTS,
+  PRODUCTS_CREATE,
+  PRODUCTS_MANAGEMENT,
   SHOP,
   SIGN_UP,
   SIGN_UP_ADMIN,
+  USERS_CREATE,
+  USERS_MANAGEMENT,
 } from "@/constants/routes"
 import { About } from "@/pages/About"
 import { Admin } from "@/pages/Admin"
 import { Cart } from "@/pages/Cart"
+import { CreateProduct } from "@/pages/CreateProduct"
+import { CreateUser } from "@/pages/CreateUser"
 import { Faq } from "@/pages/Faq"
 import { Home } from "@/pages/Home"
 import { Login } from "@/pages/Login"
@@ -27,10 +33,12 @@ import { Orders } from "@/pages/Orders"
 import { OrderSummary } from "@/pages/OrderSummary"
 import { Payment } from "@/pages/Payment"
 import { ProductDetails } from "@/pages/ProductDetails"
+import { ProductsManagement } from "@/pages/ProductsManagement"
 import { Profile } from "@/pages/Profile"
 import { Shop } from "@/pages/Shop"
 import { SignUp } from "@/pages/SignUp"
 import { User } from "@/pages/User"
+import { UsersManagement } from "@/pages/UsersManagement"
 
 export const routes = [
   { path: ABOUT, element: <About /> },
@@ -49,4 +57,8 @@ export const routes = [
   { path: CURRENT_CUSTOMER, element: <User /> },
   { path: ACCOUNT_ORDERS, element: <Orders /> },
   { path: ACCOUNT_PROFILE, element: <Profile /> },
+  { path: USERS_MANAGEMENT, element: <UsersManagement /> },
+  { path: PRODUCTS_MANAGEMENT, element: <ProductsManagement /> },
+  { path: PRODUCTS_CREATE, element: <CreateProduct /> },
+  { path: USERS_CREATE, element: <CreateUser /> },
 ]
