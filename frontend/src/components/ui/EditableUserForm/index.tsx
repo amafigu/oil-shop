85e4e310a123896d2622aacc5e6ba65287a48a4a
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-07-17 17:48:43 +0200

    delete image from user form

--- frontend/src/components/ui/EditableUserForm/index.tsx
+++ frontend/src/components/ui/EditableUserForm/index.tsx
@@ -1,17 +1,14 @@
 import { ActionButton } from "@/components/ui/ActionButton"
 import { EditableItemInput } from "@/components/ui/EditableItemInput"
-import { DEFAULT_USER_IMAGE } from "@/constants/media"
 import { STYLES } from "@/constants/styles"
 import { useUserContext } from "@/context/userContext"
 import { useTranslation } from "@/hooks/useTranslation"
 import { EditUser, User } from "@/types/User"
-import { setFileToUpload } from "@/utils/setFileToUpload"
 import { FC, MouseEvent, useState } from "react"
 import styles from "./editableUserForm.module.scss"
 
 interface EditableUserFormProps {
   item: User
-  file?: File | null | undefined
 }
 
 export const EditableUserForm: FC<EditableUserFormProps> = ({ item }) => {
@@ -19,24 +16,14 @@ export const EditableUserForm: FC<EditableUserFormProps> = ({ item }) => {
     firstName: item.firstName,
     lastName: item.lastName,
     email: item.email,
-    image: item.image,
   }
   const [updatedData, setUpdatedData] = useState<EditUser>(initialData)
-  const [file, setFile] = useState<File | undefined | null>(null)
   const { components } = useTranslation()
   const { onUpdateUser, onDeleteUser } = useUserContext()
 
   return (
     <article className={styles.wrapper} aria-label='Edit user'>
       <div className={styles.container}>
-        <div className={styles.imageContainer}>
-          <img
-            className={styles.image}
-            src={updatedData.image || DEFAULT_USER_IMAGE}
-            alt='user'
-          />
-        </div>
-
         <ActionButton
           action={(e) =>
             onDeleteUser(e as MouseEvent<HTMLButtonElement>, item.id)
@@ -52,38 +39,21 @@ export const EditableUserForm: FC<EditableUserFormProps> = ({ item }) => {
               name={key}
               updatedPropertyData={updatedData}
               onChange={(e) => {
-                if (key === "image") {
-                  setFileToUpload(e, setFile)
-                } else {
-                  setUpdatedData((prevState) => ({
-                    ...prevState,
-                    [e.target.name]: e.target.value,
-                  }))
-                }
+                setUpdatedData((prevState) => ({
+                  ...prevState,
+                  [e.target.name]: e.target.value,
+                }))
               }}
-              onSave={
-                key === "image"
-                  ? () =>
-                      onUpdateUser({
-                        key,
-                        id: item.id,
-                        initialData,
-                        updatedData,
-                        setUpdatedData,
-                        file,
-                      })
-                  : () =>
-                      onUpdateUser({
-                        key,
-                        id: item.id,
-                        initialData,
-                        updatedData,
-                        setUpdatedData,
-                      })
+              onSave={() =>
+                onUpdateUser({
+                  id: item.id,
+                  initialData,
+                  updatedData,
+                  setUpdatedData,
+                })
               }
               classCss={STYLES.FORMS.FIELD}
               type={"text"}
-              file={file}
               key={key}
             />
           ))}
