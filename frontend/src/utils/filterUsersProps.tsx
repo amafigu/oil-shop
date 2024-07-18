b8fbe5818a2adbb41acaec549908c52c00562b68
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-07-18 14:01:33 +0200

    adjust filter props

--- frontend/src/utils/filterUsersProps.tsx
+++ frontend/src/utils/filterUsersProps.tsx
@@ -2,30 +2,46 @@ import { STYLES } from "@/constants/styles"
 import { User } from "@/types/User"
 export const filterUsersProps = [
   {
-    action: (list: User[], sortIsAsc: boolean) =>
-      sortIsAsc
-        ? list.sort((a, b) => a.firstName.localeCompare(b.firstName))
-        : list.sort((a, b) => b.firstName.localeCompare(a.firstName)),
+    action: (list: User[]) =>
+      list.sort((a, b) => a.firstName.localeCompare(b.firstName)),
     targetProperty: "firstName",
-    className: STYLES.BUTTONS.ACTION,
+    className: STYLES.BUTTONS.FILTER_OPTION,
     isAsc: true,
   },
   {
-    action: (list: User[], sortIsAsc: boolean) =>
-      sortIsAsc
-        ? list.sort((a, b) => a.lastName.localeCompare(b.lastName))
-        : list.sort((a, b) => b.lastName.localeCompare(a.lastName)),
+    action: (list: User[]) =>
+      list.sort((a, b) => b.firstName.localeCompare(a.firstName)),
+    targetProperty: "firstName",
+    className: STYLES.BUTTONS.FILTER_OPTION,
+    isAsc: false,
+  },
+  {
+    action: (list: User[]) =>
+      list.sort((a, b) => a.lastName.localeCompare(b.lastName)),
     targetProperty: "lastName",
-    className: STYLES.BUTTONS.ACTION,
+    className: STYLES.BUTTONS.FILTER_OPTION,
     isAsc: true,
   },
   {
-    action: (list: User[], sortIsAsc: boolean) =>
-      sortIsAsc
-        ? list.sort((a, b) => a.email.localeCompare(b.email))
-        : list.sort((a, b) => b.email.localeCompare(a.email)),
+    action: (list: User[]) =>
+      list.sort((a, b) => b.lastName.localeCompare(a.lastName)),
+    targetProperty: "lastName",
+    className: STYLES.BUTTONS.FILTER_OPTION,
+    isAsc: false,
+  },
+  {
+    action: (list: User[]) =>
+      list.sort((a, b) => a.email.localeCompare(b.email)),
     targetProperty: "email",
-    className: STYLES.BUTTONS.ACTION,
+    className: STYLES.BUTTONS.FILTER_OPTION,
     isAsc: true,
+  },
+
+  {
+    action: (list: User[]) =>
+      list.sort((a, b) => b.email.localeCompare(a.email)),
+    targetProperty: "email",
+    className: STYLES.BUTTONS.FILTER_OPTION,
+    isAsc: false,
   },
 ]
