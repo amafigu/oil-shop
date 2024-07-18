3ba72a17b1d7a3b350bd48ff18a23ab8284c139c
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-07-18 14:04:07 +0200

    delete log updateProduct api

--- frontend/src/api/products/updateProduct.tsx
+++ frontend/src/api/products/updateProduct.tsx
@@ -2,21 +2,18 @@ import { PRODUCTS } from "@/constants/api"
 import { Product } from "@/types/Product"
 import axios from "axios"
 
-export const updateProduct = async (
-  productId: number,
-  propertyObj: Product,
-) => {
+export const updateProduct = async (productId: number, product: Product) => {
   try {
     const response = await axios.put(
       `${import.meta.env.VITE_APP_API_URL}${PRODUCTS}/${productId}`,
-      propertyObj,
+      product,
       { withCredentials: true },
     )
     if (response?.status === 200) {
       return response
     }
   } catch (error) {
-    console.error("Error by updating user", error)
+    console.error("Error by updating product", error)
     throw error
   }
 }
