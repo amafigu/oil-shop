e288ab265a2c767cc0d7504dd37d651a161edf08
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-07-18 13:57:27 +0200

    enhance sorting and styles EditableProductsList

--- frontend/src/components/ui/EditableProductsList/index.tsx
+++ frontend/src/components/ui/EditableProductsList/index.tsx
@@ -1,67 +1,74 @@
-import { ActionButton } from "@/components/ui/ActionButton"
 import { useProductContext } from "@/context/productContext"
 import { useTranslation } from "@/hooks/useTranslation"
 import { Product } from "@/types/Product"
 import { filterProductsProps } from "@/utils/filterProductsProps"
-import { FC, useEffect, useState } from "react"
-import { EditableProductForm } from "../EditableProductForm"
+import { ChangeEvent, FC, useEffect, useState } from "react"
+import { ListProductForm } from "../ListProductForm"
 import styles from "./editableProductsList.module.scss"
 
 export const EditableProductsList: FC = () => {
   const [filteredItemsList, setFilteredItemsList] = useState<Product[]>([])
-  const [isAscendent, setIsAscendent] = useState<boolean[]>(
-    filterProductsProps.map(() => true),
-  )
   const { pages } = useTranslation()
-  const productsText = pages.admin.productManagement
-  const text = pages.admin.productManagement.editableItemsList
   const { products } = useProductContext()
+  const text = pages.admin.usersManagement.editableItemsList
+
   useEffect(() => {
-    setFilteredItemsList(products)
+    setFilteredItemsList(products || [])
   }, [products])
 
-  const filterList = (
-    action: (list: Product[], sortIsAsc: boolean) => Product[],
-    index: number,
-  ) => {
-    const sortIsAsc = [...isAscendent]
-    sortIsAsc[index] = !sortIsAsc[index]
-    setIsAscendent(sortIsAsc)
-
-    const filteredItems = action(
-      [...filteredItemsList],
-      sortIsAsc[index] ?? true,
-    )
-    setFilteredItemsList(filteredItems)
+  const selectFilter = (e: ChangeEvent<HTMLSelectElement>) => {
+    const index = parseInt(e.target.value)
+    if (index >= 0 && index < filterProductsProps.length) {
+      const filterAction = filterProductsProps[index]?.action
+      if (filterAction && filteredItemsList) {
+        const sortedItems = filterAction([...filteredItemsList])
+        setFilteredItemsList(sortedItems)
+      }
+    }
   }
 
-  console.log(filterProductsProps)
   return (
-    <section className={styles.wrapper} aria-label={text.title}>
-      <h2>{productsText.editableItemsList.title}</h2>
-      <div className={styles.container}>
-        {filterProductsProps.length > 0 &&
-          filterProductsProps.map((filterButton, index) => (
-            <ActionButton
-              key={index}
-              action={() => filterList(filterButton.action, index)}
-              text={
-                isAscendent[index]
-                  ? `${text[filterButton.targetProperty]} asc`
-                  : `${text[filterButton.targetProperty]} des`
-              }
-              className={filterButton.className}
-              ariaLabel='Filter button'
-            />
-          ))}
-      </div>
-      <ul className={styles.list}>
-        {filteredItemsList.map((item, index) => (
-          <li className={styles.item} key={item.id || index}>
-            <EditableProductForm item={item} />
-          </li>
-        ))}
-      </ul>
-    </section>
+    <>
+      <section className={styles.section}>
+        <div className={styles.container}>
+          <div className={styles.filterColumn}>
+            <div className={styles.selector}>
+              <label className={styles.label} htmlFor='filterOptions'></label>
+              <select
+                className={styles.select}
+                name='filterOptions'
+                id='filterOptions'
+                onChange={selectFilter}
+                defaultValue=''
+              >
+                <option value='' disabled>
+                  {`${text.sortBy} ...`}
+                </option>
+                {filterProductsProps.map((filterButton, index) => (
+                  <option key={index} value={index}>
+                    {filterButton.isAsc
+                      ? `${text[filterButton.targetProperty]}: ${
+                          text.ascendent
+                        }`
+                      : `${text[filterButton.targetProperty]}: ${
+                          text.descendent
+                        }`}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+          <div className={styles.listColumn}>
+            <ul className={styles.list}>
+              {filteredItemsList.map((item, index) => (
+                <li className={styles.item} key={item.id || index}>
+                  <ListProductForm item={item} />
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
