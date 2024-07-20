0000a927672b7ef539d15521d59586aa3699aa3a
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-07-20 20:34:49 +0200

    add sidebar

---
+++ frontend/src/components/ui/Sidebar/index.tsx
@@ -0,0 +1,100 @@
+import { LOGO_IMAGE } from "@/constants/media"
+import { SHOP } from "@/constants/routes"
+import { useLocaleContext } from "@/context/localeContext"
+import { useTranslation } from "@/hooks/useTranslation"
+import { Category, Product } from "@/types/Product"
+import { camelCase } from "@/utils/camelCase"
+import { getIconByName } from "@/utils/getIconByName"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { Dispatch, FC, SetStateAction } from "react"
+import { Link } from "react-router-dom"
+import { ActionButton } from "../ActionButton"
+import styles from "./sidebar.module.scss"
+
+interface SidebarProps {
+  items: Product[]
+  setShowSidebar: Dispatch<SetStateAction<boolean>>
+  showSidebar: boolean
+  setItems: Dispatch<SetStateAction<string | undefined>>
+}
+
+export const Sidebar: FC<SidebarProps> = ({
+  items,
+  setShowSidebar,
+  setItems,
+  showSidebar,
+}) => {
+  const { setLanguage, language } = useLocaleContext()
+  const { components } = useTranslation()
+  const onSelect = (item: string) => {
+    setItems(item)
+    setShowSidebar(false)
+  }
+
+  const onSetLanguage = (language: "en" | "de") => {
+    setLanguage(language)
+    setShowSidebar(false)
+  }
+
+  return (
+    <nav
+      className={`${styles.sidebar} ${
+        showSidebar ? styles.visible : styles.hidden
+      }`}
+    >
+      <div className={styles.header}>
+        <div className={styles.logoContainer}>
+          <img className={styles.logo} src={LOGO_IMAGE} />
+        </div>
+        <ActionButton
+          action={() => setShowSidebar(false)}
+          className='sidebarHeader'
+        >
+          <FontAwesomeIcon icon={getIconByName("faX")} size={"sm"} />
+        </ActionButton>
+      </div>
+      <div className={styles.separator} role='separator'></div>
+      <div className={styles.listContainer}>
+        <ul className={styles.list} role='menu'>
+          {language === "en" ? (
+            <li
+              className={`${styles.listItem} ${styles.onlyMobile}`}
+              onClick={() => onSetLanguage("de")}
+            >
+              <div className={styles.listItemLink}>
+                <span className={styles.listItemLinkContent}>DE</span>
+              </div>
+            </li>
+          ) : (
+            <li
+              className={`${styles.listItem} ${styles.onlyMobile}`}
+              onClick={() => onSetLanguage("en")}
+            >
+              <div className={styles.listItemLink}>
+                <span className={styles.listItemLinkContent}>EN</span>
+              </div>
+            </li>
+          )}
+
+          {items.map((item: Category, index: number) => (
+            <li key={index} className={styles.listItem}>
+              <Link
+                onClick={() => onSelect(item.name)}
+                className={styles.listItemLink}
+                to={SHOP}
+              >
+                <span className={styles.listItemLinkContent}>
+                  {
+                    components.sidebar.productCategories[
+                      camelCase(item.name, " ")
+                    ]
+                  }
+                </span>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  )
+}
