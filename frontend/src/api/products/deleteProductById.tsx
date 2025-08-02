5b3834cfc3852c2f480dac042efc2243f9e15343
Amadeus Figueiredo <amadeusfigue@gmail.com>
2025-08-02 12:10:48 +0200

    syntex/minor change

--- frontend/src/api/products/deleteProductById.tsx
+++ frontend/src/api/products/deleteProductById.tsx
@@ -2,9 +2,10 @@ import axios, { AxiosResponse } from "axios"
 import { PRODUCTS } from "@/constants/api"
 
 export async function deleteProductById(
-  id: number
+  id: number,
 ): Promise<AxiosResponse<void>> {
   const url = `${import.meta.env.VITE_APP_API_URL}${PRODUCTS}/${id}`
+
   try {
     const response = await axios.delete<void>(url, {
       withCredentials: true,
@@ -13,8 +14,8 @@ export async function deleteProductById(
       throw new Error(`deleteProductById: Unexpected status ${response.status}`)
     }
     return response
-  } catch (err) {
-    console.error(`Error deleting product ${id}`, err)
-    throw err
+  } catch (error) {
+    console.error(`Error deleting product ${id}`, error)
+    throw error
   }
 }
